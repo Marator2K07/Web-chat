5999bb95231896719c98d0b688ea56bee9449ff5
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-22 18:29:59 +0300

    Добавлен новый компонент MiniMenu со стилем.
    Простая, пока тестовая реализация.

---
+++ frontend/src/components/MiniMenu/MiniMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './MiniMenu.module.css'
+
+export default function MiniMenu({...props}) {    
+    return (
+        <div className={classes.MiniMenu} {...props}>
+            
+        </div>
+    )
+}
