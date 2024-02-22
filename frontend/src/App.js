4f7c169e69890e630d06783859a3ec9b72af7458
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-22 19:54:31 +0300

    Update App.js
    Тестовая реализация.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import DownBlock from './components/DownBlock/DownBlock';
+import TopBlock from './components/TopBlock/TopBlock';
 
 function App() {
+
+  // заглушка
+  let userInfo = {
+    name: "Dmitry",
+    secondName: "Dmitriev",
+    image: null
+  }
+
   return (
     <div className="App">
-      
+      <TopBlock pageText="Вход в аккаунт" userInfo={userInfo}></TopBlock>
+      <DownBlock>Text</DownBlock>
     </div>
   );
 }
