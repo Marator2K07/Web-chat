ff91511e3a556575839d4dd3e45e64a51d9846ab
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-23 10:26:20 +0300

    Update AccountImageButton.jsx
    Добавлена отдельная функция для нахождения правильного расположения мини меню при появлении. Теперь есть интерактив при взаимодействии с кнопкой. Только пока элемент MiniMenu никак не реагирует на это.

--- frontend/src/components/AccountImageButton/AccountImageButton.jsx
+++ frontend/src/components/AccountImageButton/AccountImageButton.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './AccountImageButton.module.css'
 
+function setMenuOffset(idBtn, idMenu) {
+    const btnRect = document.getElementById(idBtn).getBoundingClientRect();
+    const menuElem = document.getElementById(idMenu);
+    menuElem.style.left = btnRect.left - 
+                          menuElem.getBoundingClientRect().width +
+                          btnRect.width + 10 + "px";    
+}
+
 export default function AccountImageButton({userImg, ...props}) {
+    const [hov, setHov] = useState(false);
+
     return (
         <div className={classes.AccountImageButton} {...props}>
-            <button>
+            <button id='btn' 
+                onMouseEnter={() => {
+                    setMenuOffset('btn', "miniMenu");
+                    setHov(true);
+                }}
+                onMouseLeave={() => setHov(false)}>                
                 <img src={userImg} alt="" />
                 <p>&#10094;</p>
             </button>
