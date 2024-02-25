411490c36510bbe04dbeb3f445f22427eed1cc7b
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-25 16:23:39 +0300

    Добавление нового компонента MainBlock.
    Стартовая реализация. Предполагается, что основной компонент страницы будет занимать 80 процентов высоты экрана.

---
+++ frontend/src/components/MainBlock/MainBlock.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './MainBlock.module.css'
+
+export default function MainBlock({...props}) {
+  return (
+    <div className={classes.MainBlock} {...props}>
+                        
+    </div>
+  )
+}
