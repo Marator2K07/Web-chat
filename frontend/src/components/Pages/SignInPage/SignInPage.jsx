0386223f53283b8b51a43a2634720fb91241796c
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-27 10:24:20 +0300

    Update SignInPage.jsx
    Написана начальная реализация для формы входа

--- frontend/src/components/Pages/SignInPage/SignInPage.jsx
+++ frontend/src/components/Pages/SignInPage/SignInPage.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import classes from './SignInPage.module.css'
 import TopBlock from '../../TopBlock/TopBlock'
 import DownBlock from '../../DownBlock/DownBlock'
 import MainBlock from '../../MainBlock/MainBlock'
 
-export default function SignInPage({userInfo, ...props}) {
-  return (
-    <div className={classes.SignInPage} {...props}>
-        <TopBlock pageText='Вход в аккаунт' userInfo={userInfo}/>
-        <MainBlock>
+const ApiContext = React.createContext('http://localhost:8000/api/signin');
 
-        </MainBlock>
-        <DownBlock/>
-    </div>
-  )
+export default function SignInPage({userInfo, ...props}) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(ApiContext, {
+            username,
+            password
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div className={classes.SignInPage} {...props}>
+            <TopBlock pageText='Вход в аккаунт' userInfo={userInfo}/>
+            <MainBlock>
+                <form onSubmit={handleSubmit}>
+                    <input type='username' value={username} onChange={e => setUsername(e.target.value)} placeholder='Имя аккаунта'/>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Пароль от аккаунта'/>
+                    <button type='submit'>Войти в аккаунт</button>
+                </form>
+            </MainBlock>
+            <DownBlock/>
+        </div>
+    )
 }
