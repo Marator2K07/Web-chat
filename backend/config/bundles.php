84d0fd2e186e7763be49bb3e3cd467f81e2ce4ba
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-28 16:39:52 +0300

    Установка бандла Twig.

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
