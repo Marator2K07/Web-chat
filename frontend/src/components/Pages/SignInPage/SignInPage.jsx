366d7eec90fd96ba046c3ac15996624c84222746
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-28 10:01:19 +0300

    Update SignInPage.jsx
    Более корректная реализация без критической ошибки.

--- frontend/src/components/Pages/SignInPage/SignInPage.jsx
+++ frontend/src/components/Pages/SignInPage/SignInPage.jsx
@@ -4,35 +4,44 @@ import classes from './SignInPage.module.css'
 import TopBlock from '../../TopBlock/TopBlock'
 import DownBlock from '../../DownBlock/DownBlock'
 import MainBlock from '../../MainBlock/MainBlock'
+import ResponseErrorBlock from '../../ResponseErrorBlock/ResponseErrorBlock'
 
-const ApiContext = React.createContext('http://localhost:8000/api/signin');
+const ApiUrl = 'http://127.0.0.1:8000/signin';
 
 export default function SignInPage({userInfo, ...props}) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState(null);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post(ApiContext, {
+    async function handleSubmit() {        
+        console.log(username);
+        console.log(password);
+
+        await axios.post(ApiUrl, {
             username,
             password
-            });
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
+        })
+        .then(function (response) {
+            setResponse(response);
+            setError(null);
+        })
+        .catch(function (error) {
+            setError(error);
+        })
     };
 
     return (
         <div className={classes.SignInPage} {...props}>
             <TopBlock pageText='Вход в аккаунт' userInfo={userInfo}/>
             <MainBlock>
-                <form onSubmit={handleSubmit}>
+                <form onClick={handleSubmit}>
                     <input type='username' value={username} onChange={e => setUsername(e.target.value)} placeholder='Имя аккаунта'/>
                     <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Пароль от аккаунта'/>
-                    <button type='submit'>Войти в аккаунт</button>
+                    <button type='button'>Войти в аккаунт</button>
                 </form>
+                <h3>{response === null ? '' : response.data}</h3>   
+                {error === null ? '' : <ResponseErrorBlock responseError={error}/>}            
             </MainBlock>
             <DownBlock/>
         </div>
