c0c58605c64ebdc52ca2eea465eb6af3a69a22d0
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-28 11:30:01 +0300

    Добавлены дополнительные заголовки для CORS.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000'
+    baseURL: 'http://127.0.0.1:8000',
+    headers: {
+        'Access-Control-Allow-Origin': 'http://127.0.0.1:8000',
+        'Access-Control-Allow-Methods': 'GET, POST'        
+    }
 });
 
 // в каждый запрос мы спомощью перехватчика интегрируем ключ
