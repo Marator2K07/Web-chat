78c26339e65ddff96dd3e0f61c9b83db871da142
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-28 09:17:26 +0300

    Добавили новый компонент ошибки ответа запроса.

---
+++ frontend/src/components/ResponseErrorBlock/ResponseErrorBlock.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './ResponseErrorBlock.module.css'
+
+export default function ResponseErrorBlock({resposeError, ...props}) {
+    return (
+        <div className={classes.ResponseErrorBlock} {...props}>
+            
+        </div>
+    )
+}
