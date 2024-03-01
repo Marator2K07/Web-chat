2e29aefae3964dc654d46cdde9658518c0a56b75
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-01 13:04:25 +0300

    В роутинге появилась новая страница (путь) - Registration.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Registration from './components/Pages/Registration/Registration';
 import SignInPage from './components/Pages/SignInPage/SignInPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
         <BrowserRouter>
             <Routes>
+                <Route path='/register' element={<Registration userInfo={userInfo}/>}/> 
                 <Route path='/login' element={<SignInPage userInfo={userInfo}/>}/> 
             </Routes>
         </BrowserRouter>
