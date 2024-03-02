c86c6b1d1ccfd0662cd7fedfd85bf32e4c3ad27c
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-02 15:55:19 +0300

    Создан новый раздел для компонентов Messages + первый компонент в нем.
    С начальной реализацией и пока без стиля.

---
+++ frontend/src/components/Messages/Warning/Warning.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Warning.module.css'
+
+export default function Warning() {
+  return (
+    <div className={classes.Warning} {...props}>
+                        
+    </div>
+  )
+}
