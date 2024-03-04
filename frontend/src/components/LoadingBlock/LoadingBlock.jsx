055c66b25edae3919def3d151483e2d431d08901
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-04 12:07:03 +0300

    Теперь экран загрузки берет на себя большую нагрузку.
    Выводит ошибку или информацию ответа , при наличии.

--- frontend/src/components/LoadingBlock/LoadingBlock.jsx
+++ frontend/src/components/LoadingBlock/LoadingBlock.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import classes from './LoadingBlock.module.css'
+import Info from '../Messages/Info/Info'
+import ResponseError from '../Messages/ResponseError/ResponseError'
 
-export default function LoadingBlock({innerRef, loading, ...props}) {
+export default function LoadingBlock({innerRef,
+                                      loading,
+                                      errorMsg,
+                                      responceMsg,
+                                      ...props}) {
     return (
         <div ref={innerRef} className={classes.LoadingBlock} {...props}>   
-            {loading ? <img src='LoadingIcon.png' alt=''/> : <p/>}   
+            {loading ? <img src='LoadingIcon.png' alt=''/> : <p/>}            
+            {errorMsg ? <ResponseError message={errorMsg}/> : <p/>}  
+            {responceMsg ? <Info message={responceMsg}/> : <p/>}
         </div>
     )
 }
