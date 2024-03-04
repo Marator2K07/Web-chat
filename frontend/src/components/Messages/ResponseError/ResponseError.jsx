d05da82c7c47cf46c9e4cb7699e829e6da47a4d9
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-04 16:27:24 +0300

    Добавлена кнопка и передаваемый метод для вызова handleLoading.
    А также стиль для нее.

--- frontend/src/components/Messages/ResponseError/ResponseError.jsx
+++ frontend/src/components/Messages/ResponseError/ResponseError.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import classes from './ResponseError.module.css'
 
-export default function ResponseError({message, ...props}) {
+export default function ResponseError({message, handleLoading, ...props}) {
     return (
         <div className={classes.ResponseError} {...props}>
             <div className='name'>Имя: {message.name}</div>
             <div className='code'>Код: {message.code}</div>
             <div className='info'>Сообщение: {message.message}</div>
+            <button type="button" onClick={handleLoading}>
+                Вернуться
+            </button>
         </div>
     )
 }
