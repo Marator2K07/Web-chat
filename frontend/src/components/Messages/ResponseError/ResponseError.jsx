38aad52a94383b3db76f8cbca69b081d5992e357
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-05 19:49:29 +0300

    Небольшое изменение стиля и реализации компонента.

--- frontend/src/components/Messages/ResponseError/ResponseError.jsx
+++ frontend/src/components/Messages/ResponseError/ResponseError.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import classes from './ResponseError.module.css'
 
-export default function ResponseError({message, handleLoading, ...props}) {
+export default function ResponseError({message, setLoading, ...props}) {
     return (
         <div className={classes.ResponseError} {...props}>
             <div className='name'>Имя: {message.name}</div>
             <div className='code'>Код: {message.code}</div>
             <div className='info'>Сообщение: {message.message}</div>
-            <button type="button" onClick={handleLoading}>
+            <button type="button" onClick={() => setLoading(false)}>
                 Вернуться
             </button>
         </div>
