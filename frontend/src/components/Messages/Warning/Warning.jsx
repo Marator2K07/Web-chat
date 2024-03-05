ea4495d47ee357c2050ba7349f9edad8acb9fa43
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-05 19:50:20 +0300

    Добавлена предполагаемая реализация и стиль для компонента.

--- frontend/src/components/Messages/Warning/Warning.jsx
+++ frontend/src/components/Messages/Warning/Warning.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import classes from './Warning.module.css'
 
-export default function Warning() {
-  return (
-    <div className={classes.Warning} {...props}>
-                        
-    </div>
-  )
+export default function Warning(message, setLoading, ...props) {
+    return (
+        <div className={classes.Warning} {...props}>
+            <div className='main'>Сообщение: {message.main}</div>
+            <div className='addition'>Дополнительная информация: {message.addition}</div>
+            <button type="button" onClick={() => setLoading(false)}>
+                Вернуться
+            </button>
+        </div>        
+    )
 }
