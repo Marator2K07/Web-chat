40966b3060603d72a2123f86f0f2c360c1f3a09a
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-05 16:00:39 +0300

    Теперь данный компонент является связующим среди всех main-компонентов.
    Также имеет анимированный экран загрузки с обратной связью.

--- frontend/src/components/MainBlock/MainBlock.jsx
+++ frontend/src/components/MainBlock/MainBlock.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { CSSTransition } from 'react-transition-group';
 import classes from './MainBlock.module.css'
+import LoadingBlock from '../LoadingBlock/LoadingBlock';
+import DynamicComponent from './DynamicMainBlock';
 
-export default function MainBlock({...props}) {
-  return (
-    <div className={classes.MainBlock} {...props}>
-                        
-    </div>
-  )
+export default function MainBlock({user, 
+                                   currentMainBlock,
+                                   setCurrentMainBlock,
+                                   setHeaderText,
+                                   ...props}) {
+    const [responce, setResponce] = useState(null);      
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);  
+    const nodeRef = useRef(null); // для анимации загрузки
+
+    const handleLoading = () => {        
+        setLoading(!loading);
+    }
+
+    return (
+        <div className={classes.MainBlock} {...props}>
+            <DynamicComponent component={currentMainBlock}/>
+            <CSSTransition 
+            in={loading}
+            nodeRef={nodeRef}
+            timeout={333}
+            classNames="LoadingBlock">
+                <LoadingBlock innerRef={nodeRef}
+                              loading={loading}
+                              handleLoading={handleLoading}                             
+                              error={error}  
+                              responce={responce}/>
+            </CSSTransition>  
+        </div>
+    )
 }
