a77b1ab2a115002638f894b20c0a36d5b7f4c960
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-05 15:07:30 +0300

    Улучшили нейминг параметра.

--- frontend/src/components/TopBlock/TopBlock.jsx
+++ frontend/src/components/TopBlock/TopBlock.jsx
@@ -2,14 +2,14 @@ import { React } from 'react'
 import classes from './TopBlock.module.css'
 import AccountImageButton from '../AccountImageButton/AccountImageButton';
 
-const TopBlock = ({pageText, user, ...props}) => {
+const TopBlock = ({headerText, user, ...props}) => {
     return (
         <div className={classes.TopBlock} {...props}>
             <div className='left'>
                 <p>AwesomeChat</p>
             </div>
             <div className='mid'>
-                <p>{pageText}</p>
+                <p>{headerText}</p>
             </div>
             <div className='right'>            
                 <AccountImageButton userImg={!user ? "DefUserIcon.png" : user.image}/>
