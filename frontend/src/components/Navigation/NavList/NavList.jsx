4fc7cd1abb9e247fa46400b9450a7f07fdb53245
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-05 09:49:52 +0300

    Данные рутинга можно задавать сразу в соотв. компоненте.

--- frontend/src/components/Navigation/NavList/NavList.jsx
+++ frontend/src/components/Navigation/NavList/NavList.jsx
@@ -3,6 +3,12 @@ import classes from './NavList.module.css'
 import NavItem from '../NavItem/NavItem'
 
 export default function NavList({currentRoot, ...props}) {
+    // можно сразу здесь обьявить весь рутинг
+    const data = {
+        registerRoot: {path: '/register', description: 'Регистрация'},
+        loginRoot: {path: '/login', description: 'Вход в аккаунт'}
+    }
+
     return (
         <div className={classes.NavList} {...props}>
             { Object.keys(data).map(key => (
