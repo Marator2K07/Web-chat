65f1129bc1a92465f8b70d9e64aa044583fe3ebf
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-05 14:28:45 +0300

    Переосмысление реализации. Максимальная разгрузка этого компонента.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,15 @@
 import './App.css';
-import Registration from './components/Pages/Registration/Registration';
-import SignInPage from './components/Pages/SignInPage/SignInPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import AppPage from './components/AppPage/AppPage';
 
 function App() {
+  	// заглушка
+	let user = null;
 
-  // заглушка
-  let userInfo = {
-    name: "Dmitry",
-    secondName: "Dmitriev",
-    image: null
-  }
-
-  return (    
-    <div className="App">
-        <BrowserRouter>
-            <Routes>
-                <Route path='/register' element={<Registration userInfo={userInfo}/>}/> 
-                <Route path='/login' element={<SignInPage userInfo={userInfo}/>}/> 
-            </Routes>
-        </BrowserRouter>
-    </div>
-  );
+	return (    
+		<div className="App">
+			<AppPage user={user}></AppPage>
+		</div>
+	);
 }
 
 export default App;
