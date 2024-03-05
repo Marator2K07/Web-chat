5e6f7dbfabcc14a669e2fc875c644d66499d9b90
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-05 15:51:22 +0300

    Переименовали, перенесли и усовершенствовали компонент RegisterMainBlock.
    Теперь является компонентом-ребенком MainBlock.

--- frontend/src/components/MainBlock/Components/RegisterMainBlock/RegisterMainBlock.jsx
+++ frontend/src/components/MainBlock/Components/RegisterMainBlock/RegisterMainBlock.jsx
@@ -1,22 +1,16 @@
-import React, { useState, useRef } from 'react'
-import { CSSTransition } from 'react-transition-group';
+import React, { useState } from 'react'
 import axios from 'axios'
-import classes from './Registration.module.css'
-import TopBlock from '../../TopBlock/TopBlock'
-import DownBlock from '../../DownBlock/DownBlock'
-import MainBlock from '../../MainBlock/MainBlock'
-import LoadingBlock from '../../LoadingBlock/LoadingBlock'
-import '../../LoadingBlock/LoadingBlockCSSTransition.css';
+import classes from './RegisterMainBlock.module.css'
+import '../../../LoadingBlock/LoadingBlockCSSTransition.css';
 
 const ApiUrl = 'http://127.0.0.1:8000/register';
 
-export default function Registration({userInfo, ...props}) {
+export default function RegisterMainBlock({user,
+                                           setLoading,
+                                           setResponce,
+                                           setError,
+                                           ...props}) {
     const [passwordRepeat, setPasswordRepeat] = useState(true); 
-    const [responce, setResponce] = useState(null);      
-    const [error, setError] = useState(null);
-    // для анимации загрузки
-    const [loading, setLoading] = useState(false);  
-    const nodeRef = useRef(null);
 
     // идентификационные данные
     const [credentials, setCredentials] = useState({
@@ -63,10 +57,6 @@ export default function Registration({userInfo, ...props}) {
             [e.target.name]: e.target.value
         }); 
     }
-    // вынесенная смена состояния загрузки
-    const handleLoading = () => {        
-        setLoading(!loading);
-    }
     // обработка нажатия подтверждения на форме
     async function handleSubmit(e) { 
         e.preventDefault();
@@ -100,52 +90,35 @@ export default function Registration({userInfo, ...props}) {
         })
     };
 
-    return (
-        <div className='all'>
-            <TopBlock pageText='Вход в аккаунт' userInfo={userInfo}/>            
-            <MainBlock>
-                <div className={classes.Registration} {...props}>
-                    <form name='registerForm'>
-                        <p>Придумайте имя аккаунта:</p>
-                        <input
-                        type='username'
-                        name='username'
-                        value={credentials.username}
-                        onChange={handleChange}/>
-                        <p>Задайте почту для привязки:</p>
-                        <input
-                        type='email'
-                        name='email'
-                        value={credentials.email} 
-                        onChange={handleChange}/>
-                        <p>А теперь придумайте пароль:</p>
-                        <input
-                        type='password'
-                        name='password'
-                        value={credentials.password} 
-                        onChange={handleChange}/>
-                        {passwordRepeat === true ? <p>И повторите его:</p> : <p>И повторите его (не совпадают):</p>}
-                        <input
-                        type='password'
-                        name='passwordAgain'
-                        value={credentials.passwordAgain} 
-                        onChange={handleChange}/>
-                        <button type='button' onClick={handleSubmit}>Зарегистрироваться</button>
-                    </form>                     
-                </div>                         
-            </MainBlock>  
-            <CSSTransition 
-            in={loading}
-            nodeRef={nodeRef}
-            timeout={333}
-            classNames="LoadingBlock">
-                <LoadingBlock innerRef={nodeRef}
-                              loading={loading}
-                              handleLoading={handleLoading}                             
-                              error={error}  
-                              responce={responce}/>
-            </CSSTransition>                        
-            <DownBlock/>
-        </div>
+    return (  
+        <div className={classes.RegisterMainBlock} {...props}>
+            <form name='registerForm'>
+                <p>Придумайте имя аккаунта:</p>
+                <input
+                    type='username'
+                    name='username'
+                    value={credentials.username}
+                    onChange={handleChange}/>
+                <p>Задайте почту для привязки:</p>
+                <input
+                    type='email'
+                    name='email'
+                    value={credentials.email} 
+                    onChange={handleChange}/>
+                <p>А теперь придумайте пароль:</p>
+                <input
+                    type='password'
+                    name='password'
+                    value={credentials.password} 
+                    onChange={handleChange}/>
+                {passwordRepeat === true ? <p>И повторите его:</p> : <p>И повторите его (не совпадают):</p>}
+                <input
+                    type='password'
+                    name='passwordAgain'
+                    value={credentials.passwordAgain} 
+                    onChange={handleChange}/>
+                <button type='button' onClick={handleSubmit}>Зарегистрироваться</button>
+            </form>                     
+        </div>   
     )
 }
