f283d1bc333dea133762aa3d77a69fc936b718b4
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-06 17:32:47 +0300

    Обновление реализации и стиля для компонента Info.

--- frontend/src/components/Messages/Info/Info.jsx
+++ frontend/src/components/Messages/Info/Info.jsx
@@ -4,10 +4,10 @@ import classes from './Info.module.css'
 export default function Info({message, ...props}) {
     return (
         <div className={classes.Info} {...props}>
-            <div className="main">Сообщение: {message.main}</div>
-            if (message.addition) {
-                <div className="addition">{message.addition}</div>
-            }             
+            <div className="main">Сообщение:</div>
+            <p>{message.main}</p>
+            {message.addition ? <div className="addition">Дополнительная информация:</div>  : ''} 
+            <p>{message.addition}</p>
         </div>
     )
 }
