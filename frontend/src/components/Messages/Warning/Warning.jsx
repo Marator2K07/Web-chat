1da4159048452076ea3d7607406acc27adc82d24
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-06 08:26:03 +0300

    Исправили ошибку - теперь передаем обьект.

--- frontend/src/components/Messages/Warning/Warning.jsx
+++ frontend/src/components/Messages/Warning/Warning.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './Warning.module.css'
 
-export default function Warning(message, setLoading, ...props) {
+export default function Warning({message, setLoading, ...props}) {
     return (
         <div className={classes.Warning} {...props}>
             <div className='main'>Сообщение: {message.main}</div>
