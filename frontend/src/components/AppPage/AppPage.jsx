4bf7f707c7b9732a2e6d9a07dc4e48e25dcbcab8
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-06 09:36:42 +0300

    Теперь на шаблоне страницы есть и компонент навигации.
    Плюс создана функция для контроля над навигацией.

--- frontend/src/components/AppPage/AppPage.jsx
+++ frontend/src/components/AppPage/AppPage.jsx
@@ -3,18 +3,26 @@ import classes from './AppPage.module.css'
 import TopBlock from '../TopBlock/TopBlock'
 import DownBlock from '../DownBlock/DownBlock'
 import MainBlock from '../MainBlock/MainBlock';
+import NavList from '../Navigation/NavList/NavList';
 
 export default function AppPage({user, ...props}) {  
     const [headerText, setHeaderText] = useState("Вход в аккаунт");
     const [currentMainBlock, setCurrentMainBlock] = useState('login');
     
+    function handleNavigate(path, text) {
+        setCurrentMainBlock(path);
+        setHeaderText(text);
+    }
+    
     return (
         <div className={classes.AppPage} {...props}>
-            <TopBlock headerText={headerText} user={user}/>  
-            <MainBlock user={user}
-                       currentMainBlock={currentMainBlock}
-                       setCurrentMainBlock={setCurrentMainBlock}
-                       setHeaderText={setHeaderText}/>
+            <TopBlock headerText={headerText} user={user}/>
+            <NavList handleNavigate={handleNavigate}/>  
+            <MainBlock 
+                user={user}
+                currentMainBlock={currentMainBlock}
+                setCurrentMainBlock={setCurrentMainBlock}
+                setHeaderText={setHeaderText}/>
             <DownBlock/>
         </div>
     )
