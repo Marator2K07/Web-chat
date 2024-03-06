3707ff41572a543a16528f05072dff425dafee07
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-06 09:40:51 +0300

    Незначительные правки + добавлен параметр для навигации handleNavigate.

--- frontend/src/components/Navigation/NavList/NavList.jsx
+++ frontend/src/components/Navigation/NavList/NavList.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import classes from './NavList.module.css'
 import NavItem from '../NavItem/NavItem'
 
-export default function NavList({currentRoot, ...props}) {
+export default function NavList({handleNavigate, ...props}) {
     // можно сразу здесь обьявить весь рутинг
     const data = {
-        registerRoot: {path: '/register', description: 'Регистрация'},
-        loginRoot: {path: '/login', description: 'Вход в аккаунт'}
+        registerRoot: {path: 'register', description: 'Регистрация'},
+        loginRoot: {path: 'login', description: 'Вход в аккаунт'}
     }
 
     return (
         <div className={classes.NavList} {...props}>
-            { Object.keys(data).map(key => (
+            {Object.keys(data).map(key => (
                 <div key={key}>
-                    <NavItem path={data[key].path}
-                     description={data[key].description}/>
+                    <NavItem 
+                        path={data[key].path}
+                        description={data[key].description}
+                        handleNavigate={handleNavigate}/>
                 </div>
             ))}
         </div>
