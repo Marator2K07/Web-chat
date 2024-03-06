011b4b7105df3b223fed25903f78ec9348639c1b
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-06 17:57:51 +0300

    Переименовали компонент Warning на более подходящее BadResponse.

--- frontend/src/components/Messages/BadResponse/BadResponse.jsx
+++ frontend/src/components/Messages/BadResponse/BadResponse.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import classes from './Warning.module.css'
+import classes from './BadResponse.module.css'
 
-export default function Warning({message, setLoading, ...props}) {
+export default function BadResponse({message, setLoading, ...props}) {
     return (
-        <div className={classes.Warning} {...props}>
+        <div className={classes.BadResponse} {...props}>
             <div className='main'>Сообщение: {message.main}</div>
             <div className='addition'>Дополнительная информация: {message.addition}</div>
             <button type="button" onClick={() => setLoading(false)}>
