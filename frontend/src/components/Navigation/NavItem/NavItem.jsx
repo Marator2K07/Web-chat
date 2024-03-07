aab160b4847d61a1c551e1a93a0ef3ad61c4206d
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-07 19:14:41 +0300

    Теперь для выполнения handleNavigate достаточно передать один параметр.

--- frontend/src/components/Navigation/NavItem/NavItem.jsx
+++ frontend/src/components/Navigation/NavItem/NavItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import classes from './NavItem.module.css'
 
-export default function NavItem({path,
-                                 description,
+export default function NavItem({root,
+                                 description,   
                                  handleNavigate,
                                  index,
                                  setCurrentIndex,
                                  ...props}) {
     return (
         <div className={classes.NavItem} {...props}>
-            <button onClick={() => {handleNavigate(path, description)
+            <button onClick={() => {handleNavigate(root)
                                     setCurrentIndex(index)}}>
                 {description}
             </button>            
