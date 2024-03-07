178a80e8d59703f145a606d2a70fe32659e87873
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-07 17:38:37 +0300

    Улучшение стиля компонента ResponseError.

--- frontend/src/components/Messages/ResponseError/ResponseError.jsx
+++ frontend/src/components/Messages/ResponseError/ResponseError.jsx
@@ -4,9 +4,15 @@ import classes from './ResponseError.module.css'
 export default function ResponseError({message, setLoading, ...props}) {
     return (
         <div className={classes.ResponseError} {...props}>
-            <div className='name'>Имя: {message.name}</div>
-            <div className='code'>Код: {message.code}</div>
-            <div className='info'>Сообщение: {message.message}</div>
+            <div className='name'>
+                <p>Имя: <span style={{color:'#9c5858'}}>{message.name}</span></p> 
+            </div>
+            <div className='code'>
+                <p>Код: <span style={{color:'#9c5858'}}>{message.code}</span></p> 
+            </div>
+            <div className='info'>
+                <p>Сообщение: <span style={{color:'#9c5858'}}>{message.message}</span></p> 
+            </div>
             <button type="button" onClick={() => setLoading(false)}>
                 Вернуться
             </button>
