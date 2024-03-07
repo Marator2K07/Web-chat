38e982d2b3ab7a05ceb820c641fea94dfe064f4d
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-07 19:22:49 +0300

    Доработка реализации компонента для возможной навигации или возврата.

--- frontend/src/components/Messages/OkResponse/OkResponse.jsx
+++ frontend/src/components/Messages/OkResponse/OkResponse.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import classes from './OkResponse.module.css'
 
-export default function OkResponse({message, ...props}) {
+export default function OkResponse({message,
+                                    setLoading,
+                                    handleNavigate,
+                                    ...props}) {
     return (
         <div className={classes.OkResponse} {...props}>
             <div className='main'>Сообщение:</div>
             <p>{message.main}</p>
             {message.addition ? <div className='addition'>Дополнительная информация:</div>  : ''} 
             <p>{message.addition}</p>
+            <button type="button" onClick={() => setLoading(false)}>
+                Вернуться
+            </button>
+            {message.button ? <button type="button" onClick={() => {
+                setLoading(false);
+                handleNavigate(message.button.key); 
+            }}>{message.button.text}</button> : ''}
         </div>
     )
 }
