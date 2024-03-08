b72d80d674ce24eebf1d565ddb36dabd317d35db
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-08 19:49:35 +0300

    Обновлена реализация и стиль для компонента Clue.

--- frontend/src/components/MainBlock/Minor/Clue/Clue.jsx
+++ frontend/src/components/MainBlock/Minor/Clue/Clue.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import classes from './UserCard.module.css'
+import { CSSTransition } from 'react-transition-group';
+import classes from './Clue.module.css'
+import './ClueCSSTransition.css';
 
-export default function Clue({...props}) {
-  return (
-    <div className={classes.Clue} {...props}>
-            
-    </div>
-  )
+export default function Clue({tips, ...props}) {
+    // если подсказок нет, то и отображать ничего не нужно
+    if (!tips) {        
+        return;
+    } 
+
+    return (
+        <div className={classes.Clue} {...props}>
+            {Object.keys(tips).map((key, index) => (
+                <CSSTransition
+                    key={index}
+                    in={index <= Object.keys(tips).length}
+                    timeout={444}
+                    className="Clue-init"
+                    classNames="Clue">
+                    <div key={key}>
+                        <p>{tips[key]}</p>
+                    </div>
+                </CSSTransition>
+            ))}
+        </div>
+    )
 }
