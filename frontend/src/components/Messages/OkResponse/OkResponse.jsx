44a7b1c9529008c9966421c0d3d0f902a9072088
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-08 09:40:30 +0300

    Улучшение стиля компонента OkResponse, в том числе с помощью styled-components.

--- frontend/src/components/Messages/OkResponse/OkResponse.jsx
+++ frontend/src/components/Messages/OkResponse/OkResponse.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
+import styled from 'styled-components';
 import classes from './OkResponse.module.css'
 
 export default function OkResponse({message,
                                     setLoading,
                                     handleNavigate,
                                     ...props}) {
+    const Buttons = styled.div`
+    display: flex;
+    align-self: center;
+    `;
+
     return (
         <div className={classes.OkResponse} {...props}>
-            <div className='main'>Сообщение:</div>
-            <p>{message.main}</p>
-            {message.addition ? <div className='addition'>Дополнительная информация:</div>  : ''} 
-            <p>{message.addition}</p>
-            <button type="button" onClick={() => setLoading(false)}>
-                Вернуться
-            </button>
-            {message.button ? <button type="button" onClick={() => {
-                setLoading(false);
-                handleNavigate(message.button.key); 
-            }}>{message.button.text}</button> : ''}
+            <div className='main'>
+                Сообщение:
+                <p>{message.main}</p>
+                {message.addition ? <div className='addition'>Дополнительная информация:</div>  : ''} 
+                <p>{message.addition}</p>
+            </div>            
+            <Buttons>
+                <div>
+                    <button type="button" onClick={() => setLoading(false)}>
+                        Вернуться
+                    </button>
+                </div>
+                <div>
+                    {message.button ? <button type="button" onClick={() => {
+                        setLoading(false);
+                        handleNavigate(message.button.key); 
+                    }}>{message.button.text}</button> : <p/>}
+                </div>
+            </Buttons>            
         </div>
     )
 }
