cbfe99e53c0c13b499530442dad4f55787706d72
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-08 12:14:05 +0300

    Добавлен новый компонент Clue для отображения подсказок. Начальная реализация.
    Без стиля.

---
+++ frontend/src/components/MainBlock/Minor/Clue/Clue.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './UserCard.module.css'
+
+export default function Clue({...props}) {
+  return (
+    <div className={classes.Clue} {...props}>
+            
+    </div>
+  )
+}
