1c165c448b8fb1a708d98fc3cd2bbf3fd0f550c7
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-09 16:09:15 +0300

    Создана предположительная реализация контроллера регистрации.

---
+++ backend/src/Controller/RegistrationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Service\EMailer;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register', methods: "POST")]
+    public function register(Request $request,
+                             UserPasswordHasherInterface $userPasswordHasher,   
+                             EntityManagerInterface $entityManager,
+                             EMailer $emailer): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        // задаем и инициализируем нового пользователя
+        $user = new User();
+        $user->setUsername($data['username']);
+        $user->setEmail($data['email']);
+        $user->setConfirmed(false); // по умолчанию аккаунт не активирован
+        $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                $user,
+                $data['password'] 
+            )
+        );
+        // работа с БД 
+        $entityManager->persist($user);
+        $entityManager->flush();
+        
+        $emailer->sendConfirmMessage($user);
+
+        return new JsonResponse(['status' => 'Ok',
+            'main' => 'Мы выслали подверждение на указанный при регистрации емайл.',
+            'addition' => 'Чтобы зайти под своим новым аккаунтом, нужно сначала активировать его.',
+            'button' => [
+                'text' => 'Перейти ко входу',
+                'key' => 'loginRoot'
+            ]
+        ]);
+    }
+}
