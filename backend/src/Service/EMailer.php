a15befa40c30fb3f6c9a58ac7cbff657186d1cf2
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-09 16:33:52 +0300

    Написана рабочая реализация для функции sendConfirmMessage сервиса EMailer.

--- backend/src/Service/EMailer.php
+++ backend/src/Service/EMailer.php
@@ -3,8 +3,8 @@
 namespace App\Service;
 
 use App\Entity\User;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Twig\Environment;
 
 class EMailer
@@ -16,19 +16,21 @@ class EMailer
     
     public function sendConfirmMessage(User $user)
     {  
+        // создали основной экземпляр емайл
+        $email = (new TemplatedEmail())->
+            from($this->serverEmail)->
+            to($user->getEmail())->
+            subject('Подтверждение регистрации аккаунта');
         // хэшируем ключ активации
         $userActivationKey = md5(rand().time());
-        // используем специальный шаблон для подтверждения аккаунта
-        $messageBody = $this->twig->render('confirmUser.html.twig', [
-            'user' => $user, 
-            'key' => $userActivationKey
-        ]);    
-        // составляем сообщение и отправляем    
-        $email = (new Email())
-            ->from($this->serverEmail)
-            ->to($user->getEmail())
-            ->subject('Site update just happened!')
-            ->text($messageBody);
+        // используем twig шаблон 
+        $email->htmlTemplate('confirmUser.html.twig')->
+            context([
+                'user' => $user, 
+                'key' => $userActivationKey
+            ]);         
+
+        // и наконец отправляем    
         $this->mailer->send($email);
     }
 }
