0351382565df07897dc203a4ef83689d3305e749
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-10 11:10:32 +0300

    Теперь существует еще одна страница для приложения.
    И это страница активации аккаунта.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AppPage from './components/AppPage/AppPage';
+import UserActivationPage from './components/UserActivationPage/UserActivationPage';
 
 function App() {
   	// заглушка
@@ -7,7 +9,12 @@ function App() {
 
 	return (    
 		<div className="App">
-			<AppPage user={user}></AppPage>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<AppPage user={user}/>}/> 
+					<Route path='/user_activation' element={<UserActivationPage/>}/> 
+				</Routes>
+        	</BrowserRouter>
 		</div>
 	);
 }
