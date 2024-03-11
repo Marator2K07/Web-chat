d4e20f7a810735308da116ec937db29d645f057e
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-11 10:07:59 +0300

    Убран устаревший код + усовершенствована реализация.

--- backend/src/Controller/AuthenticationController.php
+++ backend/src/Controller/AuthenticationController.php
@@ -2,33 +2,48 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 class AuthenticationController extends AbstractController
 {
-    #[Route(path: '/signin', name: 'app_sign_in')]
-    public function appSignIn(AuthenticationUtils $authenticationUtils): JsonResponse
+    #[Route(path: '/login', name: 'app_login', methods: "POST")]
+    public function login(Request $request,
+                          UserRepository $userRepository,                      
+                          UserPasswordHasherInterface $passwordHasher,
+                          JWTTokenManagerInterface $JWTManager): JsonResponse
     {
-        if ($this->getUser()) {
-            return new JsonResponse(['status' => 'Ok',
-                'message' => 'Вход в аккаунт выполнен успешно']);
+        $data = json_decode($request->getContent(), true);
+        $user = $userRepository->findOneByUsernameField($data['username']);
+
+        if (!$user) {
+            return new JsonResponse(['status' => 'Bad',
+                'main' => 'Ошибка при входе.',
+                'addition' => 'Неверные идентификационные данные.',
+                'holding' => true
+            ]);
         }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        if (!$passwordHasher->isPasswordValid($user, $data['password'])) {
+            return new JsonResponse(['status' => 'Bad',
+                'main' => 'Ошибка при входе.',
+                'addition' => 'Неверные идентификационные данные.',
+                'holding' => true
+            ]);
+        }
 
-        return new JsonResponse(['status' => 'Bad',
-            'last_username' => $lastUsername,
-            'error' => $error]);
     }
 
-    #[Route(path: '/signout', name: 'app_sign_out')]
+    #[Route(path: '/logout', name: 'app_logout')]
     public function appSignOut(Request $request): JsonResponse
     {
         $session = $request->getSession();
