5314071c2f7d839d2d755e36c2bb9703f87affb3
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-12 07:50:49 +0300

    Добавлен еще один компонент в рутинг.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AppPage from './components/Pages/AppPage/AppPage';
 import UserActivationPage from './components/Pages/UserActivationPage/UserActivationPage';
+import AfterLoginPage from './components/Pages/AfterLoginPage/AfterLoginPage';
 
 function App() {
-  	// заглушка
-	let user = null;
-
 	return (    
 		<div className="App">
 			<BrowserRouter>
 				<Routes>
-					<Route path='/' element={<AppPage user={user}/>}/> 
-					<Route path='/user_activation' element={<UserActivationPage/>}/> 
+					<Route path='/' element={<AppPage/>}/> 
+					<Route path='/user_activation' element={<UserActivationPage/>}/>
+					<Route path='/authorized_user/:username' element={<AfterLoginPage/>}/>
 				</Routes>
         	</BrowserRouter>
 		</div>
