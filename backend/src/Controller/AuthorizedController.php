f09913155cd323fb640f89d4c45fa285e121d867
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-13 21:15:58 +0300

    Создали новый контроллер для авторизованного пользователя.
    Начальная реализация.

---
+++ backend/src/Controller/AuthorizedController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorizedController extends AbstractController
+{
+    #[Route('/authorized_user/{username}', name: 'app_authorized_user', methods: 'GET')]
+    public function identity(string $username,
+                             UserRepository $userRepository): JsonResponse
+    {
+        $user = $userRepository->findOneByUsernameField($username);
+
+        if (!$user) {
+            return new JsonResponse([
+                'status' => 'Bad',
+                'main' => 'Непредвиденная ошибка.',
+                'addition' => 'Ошибка загрузки данных.'
+            ]);
+        } else {            
+            return new JsonResponse([
+                'status' => 'Ok',
+                'user' => [
+                    'id' => $user->getId(),
+                    'username' => $user->getUsername(),
+                    'roles' => $user->getRoles()
+                ]
+            ]);
+        }
+    }
+}
