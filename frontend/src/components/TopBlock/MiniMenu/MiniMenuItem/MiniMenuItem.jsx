5c660596b25347d8be0e70c1fff74053b3b5868c
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-14 19:59:37 +0300

    Дописана реализация пункта мини меню.

--- frontend/src/components/TopBlock/MiniMenu/MiniMenuItem/MiniMenuItem.jsx
+++ frontend/src/components/TopBlock/MiniMenu/MiniMenuItem/MiniMenuItem.jsx
@@ -1,14 +1,58 @@
 import React from 'react'
 import classes from './MiniMenuItem.module.css'
+import WebChatClient from '../../../../WebChatClient';
+import { useNavigate } from 'react-router-dom';
+import { useLoadingContext } from '../../../../contexts/LoadingContext/LoadingProvider';
+import { useResponseHandlerContext } from '../../../../contexts/ResponseHandlerContext/ResponseHandlerProvider';
+import { cookies } from '../../../../contexts/CookieContext';
 
 export default function MiniMenuItem({url,
                                       info,
                                       route,
-                                      extraRoute,                                      
+                                      cleanCookie,                                   
                                       ...props}) {
+    const { 
+        startLoading,
+        stopLoading, 
+        toggleHolding
+    } = useLoadingContext();
+    const { 
+        resetResult,
+        toggleResponse,
+        toggleError
+    } = useResponseHandlerContext(); 
+    const navigate = useNavigate();
+
+    const handleAction = async (e) => {
+        e.preventDefault();
+
+        // подготовка
+        startLoading();
+        resetResult();       
+
+        // внешний запрос и его обработка
+        await WebChatClient.post(url, { refreshToken: cookies.get('refreshToken') })
+        .then( async function (response) {            
+            toggleResponse(response);         
+            toggleHolding(false, 200);
+            if (cleanCookie) {
+                cookies.remove('username');
+                cookies.remove('token');
+                cookies.remove('refreshToken');
+            }
+            if (route) {
+                navigate(route, { replace: true });
+            }
+        })
+        .catch(function (error) {
+            toggleError(error);
+        });
+        stopLoading();
+    } 
+
     return (
         <div className={classes.MiniMenuItem} {...props}>
-            <button type="button">
+            <button type="button" onClick={handleAction}>
                 { info }
             </button>
         </div>
