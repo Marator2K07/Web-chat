8744f17d70f0f40ae64f2e1690da2fb128a5eeed
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-14 12:59:08 +0300

    Немного расширили функционал контекста для пользователя.

--- frontend/src/contexts/UserContext/UserContext.js
+++ frontend/src/contexts/UserContext/UserContext.js
@@ -3,10 +3,17 @@ import { useCallback, useState } from "react"
 export const useCreateUserContext = function(props) {
     // если не передадим в провайдере значение, то умолчанию null 
     const [user, setUser] = useState(props.user || null);
+    const [aboutUser, setAboutUser] = useState(null);
 
     const toggleUser = useCallback((user) => {
         setUser(user);
     }, []);
 
-    return { user, toggleUser };
+    const toggleAboutUser = useCallback((aboutUser) => {
+        setAboutUser(aboutUser);
+    }, []);
+
+    return { user,
+             toggleUser,
+             toggleAboutUser };
 }
