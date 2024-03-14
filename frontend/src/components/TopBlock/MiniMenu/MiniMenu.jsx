659cba21a81d60258d8bede76a19bbfe7133beb3
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-14 08:47:41 +0300

    Теперь компонент содержит объекты в виде пунктов меню.
    + адаптирован стиль под их отображение.

--- frontend/src/components/TopBlock/MiniMenu/MiniMenu.jsx
+++ frontend/src/components/TopBlock/MiniMenu/MiniMenu.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
 import classes from './MiniMenu.module.css'
+import MiniMenuItem from './MiniMenuItem/MiniMenuItem';
 
 const loginUrl = '/logout';
 const getTokensUrl = '/api/token/invalidate';
 
-export default function MiniMenu({innerRef, ...props}) { 
+export default function MiniMenu({innerRef,
+                                  user,
+                                  ...props}) { 
     const items = {
-
-    }
+        infoItem: { 
+            url: '/',
+            info: 'О приложении',
+            route: '/',
+            extraRoute: '/'
+        },
+        logoutItem: { 
+            url: '/',
+            info: 'Выйти из аккаунта',
+            route: loginUrl,
+            extraRoute: getTokensUrl
+        } 
+    };
     
     return (
         <div ref={innerRef} className={classes.MiniMenu} {...props}>
-            
+            {Object.keys(items).map((key) => (
+                <div key={key}>
+                    <MiniMenuItem
+                        url={items[key].url}
+                        info={items[key].info}
+                        route={items[key].route}
+                        extraRoute={items[key].extraRoute}/>
+                </div>
+            ))}
         </div>
     )
 }
