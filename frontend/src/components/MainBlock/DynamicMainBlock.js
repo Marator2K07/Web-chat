8c9f09cc9b90d8f0c0229fc9c192702e5af782f4
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-15 19:54:11 +0300

    Добавление нового компонента для динамической загрузки.

--- frontend/src/components/MainBlock/DynamicMainBlock.js
+++ frontend/src/components/MainBlock/DynamicMainBlock.js
@@ -2,12 +2,14 @@ import React from "react";
 import LoginMainBlock from "./Elements/LoginMainBlock/LoginMainBlock";
 import RegisterMainBlock from "./Elements/RegisterMainBlock/RegisterMainBlock";
 import WelcomeMainBlock from "./Elements/WelcomeMainBlock/WelcomeMainBlock";
+import PersonalPageMainBlock from "./Elements/PersonalPageMainBlock/PersonalPageMainBlock";
 
 // основные страницы/блоки приложения
 const components = {
     register: RegisterMainBlock,
     login: LoginMainBlock,
-    welcome: WelcomeMainBlock 
+    welcome: WelcomeMainBlock,
+    personalPage: PersonalPageMainBlock 
 }
 // посредник, через который мы сможем рендерить нужный компонент
 function DynamicComponent(props) {
