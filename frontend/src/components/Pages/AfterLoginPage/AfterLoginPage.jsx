31f37201b70bb5dbd4d9bf5d48f75c7a99c9dee5
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-15 06:24:32 +0300

    Обновленная реализация с возвратом на страницу входа в случае ошибок.

--- frontend/src/components/Pages/AfterLoginPage/AfterLoginPage.jsx
+++ frontend/src/components/Pages/AfterLoginPage/AfterLoginPage.jsx
@@ -1,18 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './AfterLoginPage.module.css'
 import TopBlock from '../../TopBlock/TopBlock';
 import NavList from '../../Navigation/NavList/NavList';
 import MainBlock from '../../MainBlock/MainBlock';
 import DownBlock from '../../DownBlock/DownBlock';
+import { useLocation, useNavigate } from 'react-router-dom';
+import WebChatClient from '../../../WebChatClient';
+import { useUserContext } from '../../../contexts/UserContext/UserProvider';
+import { useLoadingContext } from '../../../contexts/LoadingContext/LoadingProvider';
+import { useResponseHandlerContext } from '../../../contexts/ResponseHandlerContext/ResponseHandlerProvider';
 
-export default function AfterLoginPage({user, ...props}) {
-    // можно сразу объявить весь рутинг
+export default function AfterLoginPage({...props}) {
     const pagesData = {
-        loginRoot: {path: 'login', description: 'Вход в аккаунт', index: 0}
+        welcomeRoot: {path: 'welcome', description: 'Добро пожаловать', index: 0}
     } 
-    const [headerText, setHeaderText] = useState("Вход в аккаунт");
-    const [currentMainBlock, setCurrentMainBlock] = useState('login');
-    const [currentIndex, setCurrentIndex] = useState(null);  
+
+    const { 
+        startLoading,
+        stopLoading, 
+        toggleHolding
+    } = useLoadingContext();
+    const { 
+        resetResult,
+        toggleResponse,
+        toggleError
+    } = useResponseHandlerContext();
+    const { toggleUser } = useUserContext();
+    const [headerText, setHeaderText] = useState('Добро пожаловать');
+    const [currentMainBlock, setCurrentMainBlock] = useState('welcome');
+    const [currentIndex, setCurrentIndex] = useState(null); 
+    const location = useLocation(); 
+    const navigate = useNavigate();     
 
     const handleNavigate = (key) => {
         setCurrentMainBlock(pagesData[key].path);
@@ -20,15 +38,41 @@ export default function AfterLoginPage({user, ...props}) {
         setCurrentIndex(pagesData[key].index);
     }
 
+    // первым делом подгружаем все данные о пользователе
+    const updateUser = async () => {
+        // подготовка
+        startLoading();
+        resetResult();
+        // сам запрос и его обработка
+        await WebChatClient.get(location.pathname)
+        .then(function (response) {      
+            toggleResponse(response);
+            toggleUser(response.data.user);
+            toggleHolding(false, 1000)
+        })
+        .catch(function (error) {
+            toggleError(error);
+            setTimeout(() => {
+                navigate('/');  
+            }, 1500);
+        });
+        stopLoading();
+    }
+
+    useEffect(() => {
+        updateUser();
+        setCurrentIndex(0);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <div className={classes.AfterLoginPage} {...props}>
-            <TopBlock headerText={headerText} aboutUser={null}/>
+            <TopBlock headerText={headerText}/>
             <NavList
                 handleNavigate={handleNavigate}
                 pagesData={pagesData}
                 currentIndex={currentIndex}/>  
-            <MainBlock 
-                user={user}
+            <MainBlock
                 handleNavigate={handleNavigate}
                 currentMainBlock={currentMainBlock}/>
             <DownBlock/>
