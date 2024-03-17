123e2940b85e7bf7c884ca7ffc48902373971b90
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-17 15:46:10 +0300

    Выделили отдельный файл под константы.
    Начальная реализация.

---
+++ frontend/src/constants.js
@@ -0,0 +1,7 @@
+export const LOGIN_URL = "/api/login_check";
+
+export const AFTER_LOGIN_ROUTE = "/authorized_user"
+
+export const SHORT_DELAY = 1000;
+export const MEDIUM_DELAY = 1750;
+export const LONG_DELAY = 2500;
