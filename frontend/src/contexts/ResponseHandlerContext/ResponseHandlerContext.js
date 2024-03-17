7bbcdc02784f7057bfb864e4a60015148e2ceef7
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-17 16:07:09 +0300

    Добавили новый метод для обработки post запросов в контекст ответа.

--- frontend/src/contexts/ResponseHandlerContext/ResponseHandlerContext.js
+++ frontend/src/contexts/ResponseHandlerContext/ResponseHandlerContext.js
@@ -1,12 +1,35 @@
 import { useCallback, useState } from "react"
+import WebChatClient from "../../WebChatClient";
 
-export const useCreateResponseHandlerContext = function() {
+export const useCreateResponseHandlerContext = function() {    
     const [response, setResponse] = useState(null);
     const [error, setError] = useState(null);
 
     const resetResult = useCallback(() => {
         setResponse(null);
         setError(null);
+    }, []);
+
+    const makePostRequest = useCallback(async (
+                                        url,
+                                        data,
+                                        responseFunc = null,
+                                        errorFunc = null
+                                        ) => {        
+        await WebChatClient.post(url, data)
+        .then(function (response) {
+            setResponse(response);
+            if (responseFunc) {
+                responseFunc(response);
+            }  
+        })
+        .catch(function (error) {
+            setError(error);
+            console.log(error);
+            if (errorFunc) {
+                errorFunc();
+            }
+        });
     }, []);
 
     const toggleResponse = useCallback((response) => {
@@ -22,4 +45,5 @@ export const useCreateResponseHandlerContext = function() {
              resetResult,
              toggleResponse,
              toggleError };
+             makePostRequest };
 }
