92ed6d47b6deac86946529eda1c1951774c126e8
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-18 07:54:20 +0300

    Применение анимации перехода от компонента AppAnimatedLayout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,37 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import BeforeLoginPage from './components/Pages/BeforeLoginPage/BeforeLoginPage';
 import UserActivationPage from './components/Pages/UserActivationPage/UserActivationPage';
 import AfterLoginPage from './components/Pages/AfterLoginPage/AfterLoginPage';
 import { UserProvider } from './contexts/UserContext/UserProvider';
 import { LoadingProvider } from './contexts/LoadingContext/LoadingProvider';
 import { ResponseHandlerProvider } from './contexts/ResponseHandlerContext/ResponseHandlerProvider';
+import { AnimatePresence } from 'framer-motion';
+import './App.css';
 
 function App() {
+	const location = useLocation();
+
 	return (    
 		<div className="App">
 			<LoadingProvider>
 			<ResponseHandlerProvider>
-				<Routes>						
-						<Route path='/user_activation' element={ <UserActivationPage/> }/>
-						<Route path='/' element={
-							<UserProvider>
-								<BeforeLoginPage/>
-							</UserProvider>
-						}/> 
-						<Route path='/authorized_user/:username' element={
-							<UserProvider>
-								<AfterLoginPage/>
-							</UserProvider>						
-						}/>
+			<AnimatePresence mode='wait' initial={false}>				
+				<Routes location={location} key={location.pathname}>
+					<Route path="/" element={
+						<UserProvider>
+							<BeforeLoginPage/>
+						</UserProvider>						
+					}/> 
+					<Route path="user_activation" element={<UserActivationPage/>}/>
+					<Route path="authorized_user/:username" element={
+						<UserProvider>
+							<AfterLoginPage/>
+						</UserProvider>												
+					}/>					
 				</Routes>
+			</AnimatePresence>	
 			</ResponseHandlerProvider>				
-			</LoadingProvider>				
+			</LoadingProvider>
 		</div>
 	);
 }
--- frontend/src/components/MainBlock/DynamicMainBlock.js
+++ frontend/src/components/MainBlock/DynamicMainBlock.js
@@ -3,6 +3,7 @@ import LoginMainBlock from "./Elements/LoginMainBlock/LoginMainBlock";
 import RegisterMainBlock from "./Elements/RegisterMainBlock/RegisterMainBlock";
 import WelcomeMainBlock from "./Elements/WelcomeMainBlock/WelcomeMainBlock";
 import PersonalPageMainBlock from "./Elements/PersonalPageMainBlock/PersonalPageMainBlock";
+import { AppAnimatedLayout } from "../../AppAnimatedLayout";
 
 // основные страницы/блоки приложения
 const components = {
@@ -14,7 +15,9 @@ const components = {
 // посредник, через который мы сможем рендерить нужный компонент
 function DynamicComponent(props) {
     const SelectedComponent = components[props.component];
-    return <SelectedComponent/>
+    return <AppAnimatedLayout>
+               <SelectedComponent/>
+           </AppAnimatedLayout>
 }
 
 export default DynamicComponent;
