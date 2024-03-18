79cdf4e04252bab09563c1fa023c6f43cdc922bb
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-18 09:33:30 +0300

    Настройка контроллера к новой реализации фронта.
    Теперь есть только два варианта ответа - ошибка или успешно.

--- backend/src/Controller/AuthenticationController.php
+++ backend/src/Controller/AuthenticationController.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,36 +21,19 @@ class AuthenticationController extends AbstractController
         $user = $userRepository->findOneByUsernameField($data['username']);
 
         if (!$user) {
-            return new JsonResponse([
-                'status' => 'Bad',
-                'main' => 'Ошибка при входе.',
-                'addition' => 'Неверные идентификационные данные.',
-                'holding' => true
-            ]);
+            throw new HttpException(401, 'Неверные идентификационные данные');
         }
 
         if (!$passwordHasher->isPasswordValid($user, $data['password'])) {
-            return new JsonResponse([
-                'status' => 'Bad',
-                'main' => 'Ошибка при входе.',
-                'addition' => 'Неверные идентификационные данные.',
-                'holding' => true
-            ]);
+            throw new HttpException(401, 'Неверные идентификационные данные');
         }
 
         if (!$user->isConfirmed()) {
-            return new JsonResponse([
-                'status' => 'Bad',
-                'main' => 'Ошибка при входе.',
-                'addition' => 'Аккаунт не активирован.',
-                'holding' => true
-            ]);
+            throw new HttpException(409, 'Аккаунт не активирован');
         } else {
             return new JsonResponse([
                 'status' => 'Ok',
-                'main' => 'Успешный вход.',
-                'next_stage' => true, // даем согласие на оставшуюся часть запроса
-                'link' => '/authorized_user/'.$user->getUsername()
+                'main' => 'Успешный вход.'
             ]);  
         }         
     }
