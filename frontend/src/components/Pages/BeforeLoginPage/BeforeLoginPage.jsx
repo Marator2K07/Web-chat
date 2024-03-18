9d271cd2b054b6925355099f555adc982872b145
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-18 21:33:15 +0300

    Усовершенствовали метод навигации - теперь он учитывает анимацию.

--- frontend/src/components/Pages/BeforeLoginPage/BeforeLoginPage.jsx
+++ frontend/src/components/Pages/BeforeLoginPage/BeforeLoginPage.jsx
@@ -4,21 +4,38 @@ import TopBlock from '../../TopBlock/TopBlock'
 import DownBlock from '../../DownBlock/DownBlock'
 import MainBlock from '../../MainBlock/MainBlock';
 import NavList from '../../Navigation/NavList/NavList';
+import { useMainBlockAnimationContext } from '../../../contexts/MainBlockAnimationContext/MainBlockAnimationProvider';
+import { EXTRA_SHORT_DELAY } from '../../../constants';
 
 export default function BeforeLoginPage({...props}) {
     const pagesData = {
         loginRoot: {path: 'login', description: 'Вход в аккаунт', index: 0},
         registerRoot: {path: 'register', description: 'Регистрация', index: 1}
     } 
+    const { 
+        initCondition,
+        leftCondition,
+        rightCondition
+    } = useMainBlockAnimationContext();
     
     const [headerText, setHeaderText] = useState("Вход в аккаунт");
     const [currentMainBlock, setCurrentMainBlock] = useState('login');
     const [currentIndex, setCurrentIndex] = useState(null); 
     
+    // переход между вкладками с анимацией
     const handleNavigate = (key) => {
-        setCurrentMainBlock(pagesData[key].path);
-        setHeaderText(pagesData[key].description);
-        setCurrentIndex(pagesData[key].index);
+        currentIndex < pagesData[key].index ? leftCondition()
+                                            : rightCondition();
+        setTimeout(() => {
+            setCurrentMainBlock(pagesData[key].path);
+            setHeaderText(pagesData[key].description);
+            setCurrentIndex(pagesData[key].index);
+            currentIndex < pagesData[key].index ? rightCondition() 
+                                                : leftCondition();
+            setTimeout(() => {
+                initCondition();
+            }, EXTRA_SHORT_DELAY);
+        }, EXTRA_SHORT_DELAY);        
     }
 
     useEffect(() => {
