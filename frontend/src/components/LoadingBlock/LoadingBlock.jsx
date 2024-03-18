2551ff4dbba0c67d3dabac3d10b8130ab9625ba2
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-18 08:18:31 +0300

    Улучшение синтаксиса, использование констант + теперь max opacity - 0.9

--- frontend/src/components/LoadingBlock/LoadingBlock.jsx
+++ frontend/src/components/LoadingBlock/LoadingBlock.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import classes from './LoadingBlock.module.css'
 import ResponseError from '../Messages/ResponseError/ResponseError'
-import BadResponse from '../Messages/BadResponse/BadResponse'
 import OkResponse from '../Messages/OkResponse/OkResponse'
 import { Spin } from 'antd'
 import { SyncOutlined } from '@ant-design/icons'
 import { useLoadingContext } from '../../contexts/LoadingContext/LoadingProvider'
 import { useResponseHandlerContext } from '../../contexts/ResponseHandlerContext/ResponseHandlerProvider'
+import { SALMON_COLOR } from '../../constants'
 
 export default function LoadingBlock({innerRef,
                                       handleNavigate,
@@ -17,28 +17,22 @@ export default function LoadingBlock({innerRef,
     return (
         <div ref={innerRef} className={classes.LoadingBlock} {...props}>   
             {
-                loading && <Spin
-                size='large'
-                indicator={<SyncOutlined spin style={{ 
-                    fontSize: 44, color: "rgb(255, 140, 100)"
-                }}/>}/>
+                loading && 
+                <Spin
+                    size='large'
+                    indicator={<SyncOutlined spin style={{ 
+                        fontSize: 44, color: SALMON_COLOR
+                    }}/>}
+                />
             } 
-            {error && <ResponseError message={error}/>}    
-            {
-                (() => {
-                    if (response) {
-                        switch (response.data.status) {
-                            case 'Ok':
-                                return <OkResponse
-                                    handleNavigate={handleNavigate}
-                                    message={response.data}/>
-                            case 'Bad': 
-                                return <BadResponse message={response.data}/>    
-                            default:
-                                return <p>...</p>                                  
-                        }
-                    }
-                })()
+            { error && <ResponseError message={error}/> }   
+                {console.log(response)} 
+            { 
+                response && 
+                response.data && 
+                <OkResponse
+                    handleNavigate={handleNavigate}
+                    message={response.data}/>
             }
         </div>
     )
