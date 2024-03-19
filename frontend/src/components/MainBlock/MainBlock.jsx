d425ce5662a7a51232bf73d6132a3e9c6b2946b0
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-19 11:41:43 +0300

    Убран по возможности хардкод + добавили анимацию основному компоненту.
    + обновления в файле констант.

--- frontend/src/components/MainBlock/MainBlock.jsx
+++ frontend/src/components/MainBlock/MainBlock.jsx
@@ -3,26 +3,23 @@ import { CSSTransition } from 'react-transition-group';
 import classes from './MainBlock.module.css'
 import LoadingBlock from '../LoadingBlock/LoadingBlock';
 import DynamicComponent from './DynamicMainBlock';
+import { motion } from "framer-motion";
 import { cookies } from '../../contexts/CookieContext';
 import { useNavigate } from 'react-router-dom';
 import { useLoadingContext } from '../../contexts/LoadingContext/LoadingProvider';
-
-// в случае если пользователь уже зашел в аккаунт
-const jumpUrl = '/authorized_user';
+import { useMainBlockAnimationContext } from '../../contexts/MainBlockAnimationContext/MainBlockAnimationProvider';
+import { AFTER_LOGIN_ROUTE, SHORT_DELAY, SHORT_TIMEOUT } from '../../constants';
 
 export default function MainBlock({handleNavigate, 
                                    currentMainBlock,
                                    ...props}) {
-    const { 
-        holding,
-        startLoading,
-        stopLoading, 
-        toggleHolding
-    } = useLoadingContext();
+    const { holding, startLoading, stopLoading } = useLoadingContext();
+    const { x, opacity, duration } = useMainBlockAnimationContext();
     const nodeRef = useRef(null);  
     const navigate = useNavigate();
 
-    // автоматический плавный переход на страницу аккаунта
+    // автоматический переход на страницу аккаунта в случае 
+    // не выхода из аккаунта и закрытии браузера
     useEffect(() => {
         let username = cookies.get('username');
         let token = cookies.get('token');
@@ -30,25 +27,30 @@ export default function MainBlock({handleNavigate,
             startLoading();
             setTimeout(() => {   
                 stopLoading();             
-                navigate(`${jumpUrl}/${username}`, { replace: true });                
-            }, 700);            
-            toggleHolding(false, 700);
+                navigate(`${AFTER_LOGIN_ROUTE}/${username}`, { replace: true });                
+            }, SHORT_DELAY);
         }   
     }, [
         navigate,
         currentMainBlock,
         startLoading,
-        stopLoading,
-        toggleHolding
+        stopLoading
     ]);    
 
     return (
         <div className={classes.MainBlock} {...props}>
-            <DynamicComponent component={currentMainBlock}/>
-            <CSSTransition 
+            <motion.div
+            animate={{ x: x, opacity: opacity }}
+            transition={{
+              duration: duration,
+              ease: [0.11, 0.9, 0.4, 1.11]
+            }}>
+                <DynamicComponent component={currentMainBlock}/>
+            </motion.div>
+            <CSSTransition
                 in={holding}
                 nodeRef={nodeRef}
-                timeout={333}
+                timeout={SHORT_TIMEOUT}
                 classNames="LoadingBlock">
                 <LoadingBlock 
                     innerRef={nodeRef}
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -4,11 +4,16 @@ export const REGISTER_URL = "/register";
 export const USER_ACTIVATION_URL = "/user_activation";
 
 export const AFTER_LOGIN_ROUTE = "/authorized_user"
+export const BEFORE_LOGIN_ROUTE = "/"
 
 export const EXTRA_SHORT_DELAY = 250;
-export const SHORT_DELAY = 1500;
+export const SHORT_DELAY = 1250;
 export const MEDIUM_DELAY = 2500;
 export const LONG_DELAY = 3500;
+
+export const SHORT_TIMEOUT = 350;
+export const MEDIUM_TIMEOUT = 550;
+export const LONG_TIMEOUT = 850;
 
 export const FIVE_MIN_AGE = 3600;
 export const ONE_MONTH_AGE = 2592000;
