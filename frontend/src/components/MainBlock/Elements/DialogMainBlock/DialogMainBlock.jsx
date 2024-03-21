3a835424ced602759d37b5d8305077a1bc12d3a5
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-21 09:33:44 +0300

    Добавили новый компонент для отображения участка для диалогов.
    Стартовая реализация.

---
+++ frontend/src/components/MainBlock/Elements/DialogMainBlock/DialogMainBlock.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './DialogMainBlock.module.css'
+
+export default function DialogMainBlock({...props}) {
+  return (
+    <div className={classes.DialogMainBlock} {...props}>
+        DialogMainBlock
+    </div>
+  )
+}
