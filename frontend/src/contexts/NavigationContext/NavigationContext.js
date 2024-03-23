fa40c56e2e87873f54a6b5460f4e64b7923b0d59
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-23 11:22:33 +0300

    Контекст навигации теперь содержит в себе все необходимые данные.

--- frontend/src/contexts/NavigationContext/NavigationContext.js
+++ frontend/src/contexts/NavigationContext/NavigationContext.js
@@ -1,18 +1,52 @@
 import { useCallback, useState } from "react"
+import { useMainBlockAnimationContext } from "../MainBlockAnimationContext/MainBlockAnimationProvider";
+import { EXTRA_SHORT_DELAY } from "../../constants";
 
-export const useCreateNavigationContext = function() {
-    const [headerText, setHeaderText] = useState();
-    const [mainBlock, setMainBlock] = useState();
-    const [index, setIndex] = useState();    
+export const useCreateNavigationContext = function() {   
+    const [blocksData] = useState({
+        loginBlock: {path: 'login', description: 'Вход в аккаунт', index: 0},
+        registerBlock: {path: 'register', description: 'Регистрация', index: 1},
+        welcomeBlock: {path: 'welcome', description: 'Добро пожаловать', index: 2},
+        personalBlock: {path: 'personal', description: 'Личная страница', index: 3},
+        communicationBlock: {path: 'communication', description: 'Общение', index: 4}
+    }) 
+    const {initCondition, leftCondition, rightCondition} = useMainBlockAnimationContext();
+    const [headerText, setHeaderText] = useState('Вход в аккаунт');
+    const [mainBlock, setMainBlock] = useState('login');
+    const [index, setIndex] = useState('0');   
 
-    const goNavigation = useCallback((header, block, index) => {
-        setHeaderText(header);
-        setMainBlock(block);
-        setIndex(index);
-    }, []);
+    const goNavigation = useCallback((key) => {
+        setHeaderText(blocksData[key].description);
+        setMainBlock(blocksData[key].path);
+        setIndex(blocksData[key].index);
+    }, [blocksData]);
+
+    const goNavigationWithAnimation = useCallback((key) => {
+        console.log(key);
+        index < blocksData[key].index ? leftCondition()
+                                      : rightCondition();
+        setTimeout(() => {
+            setHeaderText(blocksData[key].description);
+            setMainBlock(blocksData[key].path);
+            setIndex(blocksData[key].index);
+            index < blocksData[key].index ? rightCondition() 
+                                          : leftCondition();
+            setTimeout(() => {
+                initCondition();
+            }, EXTRA_SHORT_DELAY);
+        }, EXTRA_SHORT_DELAY);  
+    }, [
+        blocksData,
+        index,
+        initCondition,
+        leftCondition,
+        rightCondition
+    ]);
 
     return { headerText,
              mainBlock,
              index,
-             goNavigation };
+             blocksData,
+             goNavigation,
+             goNavigationWithAnimation };
 }
