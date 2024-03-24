551cd5c38d1ba0701376b375d3a66ba4cb0a7247
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-24 18:47:13 +0300

    Существенное пополнение реализации и написание стиля.
    + обновление файла констант.

--- frontend/src/components/MessagesBlock/MessagesBlock.jsx
+++ frontend/src/components/MessagesBlock/MessagesBlock.jsx
@@ -3,21 +3,61 @@ import classes from './MessagesBlock.module.css'
 import { useUserContext } from '../../contexts/UserContext/UserProvider'
 import MessagesCollection from '../Collection/MessagesCollection/MessagesCollection';
 import { useResponseHandlerContext } from '../../contexts/ResponseHandlerContext/ResponseHandlerProvider';
-import { GET_MESSAGES_FOR_ROOM_URL } from '../../constants';
+import { GET_MESSAGES_FOR_ROOM_URL, MEDIUM_DELAY, NEW_MESSAGE_URL } from '../../constants';
+import { useLoadingContext } from '../../contexts/LoadingContext/LoadingProvider';
+import Scrollable from '../Collection/Scrollable/Scrollable';
 
 export default function MessagesBlock({...props}) {
-    const { resetResult, makeGetRequest } = useResponseHandlerContext();
-    const { roomForNews } = useUserContext();
+    const { toggleHolding, startLoading, stopLoading } = useLoadingContext();
+    const {
+        resetResult,
+        makeGetRequest,
+        makePostRequest
+    } = useResponseHandlerContext();
+    const { user, roomForNews } = useUserContext();
     const [messages, setMessages] = useState({});
+    const [count, setCount] = useState();
+
+    // для отслеживания нового сообщения
+    const [newMessage, setNewMessage] = useState(''); 
+
+    // установка изменений в идентификационных данных
+    const handleChange = (e) => {
+        setNewMessage(e.target.value);
+    }
+
+    // обработка отправки сообщения
+    const handleSend = async() => {
+        startLoading();
+        resetResult();
+        await makePostRequest(
+            `${NEW_MESSAGE_URL}`,
+            { 
+                username: user.username,
+                roomId: roomForNews.id,
+                information: newMessage
+            },
+            (response) => {
+                setMessages({
+                    ...messages,
+                    [count]: response.data.message
+                });
+                toggleHolding(false, MEDIUM_DELAY);
+                setCount(count+1);
+            },
+            toggleHolding(false, MEDIUM_DELAY)
+        )
+        stopLoading();
+    }
 
     // подгрузка сообщений
     const updateMessages = async() => {
         resetResult();
         await makeGetRequest(
-            `${GET_MESSAGES_FOR_ROOM_URL}/${roomForNews.id}`,
+            `${GET_MESSAGES_FOR_ROOM_URL}/${roomForNews.id}`,            
             (response) => {
                 setMessages(response.data.messages);
-                console.log(response.data.messages);
+                setCount(response.data.messages.lenght)
             }
         )
     }
@@ -28,10 +68,20 @@ export default function MessagesBlock({...props}) {
 
     return (
         <div className={classes.MessagesBlock} {...props}>            
-            {roomForNews.name}
-            <MessagesCollection
-                messages={messages}
-                clue={'...Нет ни одного сообщения...'}/>
+            <h4>{roomForNews.name}</h4>
+            <Scrollable>
+                <MessagesCollection
+                    messages={messages}
+                    clue={'...Нет ни одного сообщения...'}/>
+            </Scrollable>                
+            <input
+                type='text'
+                name='message'
+                value={newMessage}
+                onChange={handleChange}/>
+            <button type='button' onClick={handleSend}>
+                Поделиться
+            </button>
         </div>
     )
 }
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -8,6 +8,7 @@ export const GET_USER_NEWS_MESSAGES_URL = "/authorized_user/news/messages/get";
 export const GET_MESSAGES_FOR_ROOM_URL = "/message/all/get"
 export const USERS_SEARCH_URL = "/user/all/search";
 export const GET_OTHER_USER_URL  = "/user/get"
+export const NEW_MESSAGE_URL = "/message/new"
 
 export const OTHER_USER_ROUTE = "/other_user"
 export const AFTER_LOGIN_ROUTE = "/authorized_user"
