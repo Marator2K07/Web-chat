31646e2383e9936fe6c72aa2233d4fd12f0f6b0b
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-24 18:50:58 +0300

    Предположительные: реализация и стиль.

--- frontend/src/components/MainBlock/Elements/NewsMainBlock/NewsMainBlock.jsx
+++ frontend/src/components/MainBlock/Elements/NewsMainBlock/NewsMainBlock.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import classes from './NewsMainBlock.module.css'
+import MessagesBlock from '../../../MessagesBlock/MessagesBlock'
 
 export default function NewsMainBlock({...props}) {
     return (
-        <div className={classes.NewsMainBlock} {...props}>
-            NewsMainBlock
+        <div className={classes.NewsMainBlock} {...props}>            
+            <MessagesBlock/>
         </div>
     )
 }
