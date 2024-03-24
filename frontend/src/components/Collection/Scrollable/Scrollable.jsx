1223f2fd63f7ae4e611d06b14edc095228aadd9d
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-24 18:46:35 +0300

    Добавлен компонент прослойка для возможности прокрутки конкретного элемента.

---
+++ frontend/src/components/Collection/Scrollable/Scrollable.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Scrollable.module.css'
+
+export default function Scrollable({...props}) {
+    return (
+        <div className={classes.Scrollable} {...props}>
+
+        </div>
+    )
+}
