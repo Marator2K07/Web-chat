14408e96ec40addec025d59ee14ad3185a8301cf
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-24 18:42:37 +0300

    Небольшие правки в контроллере + новый метод для создания сообщения в комнате.

--- backend/src/Controller/MessageController.php
+++ backend/src/Controller/MessageController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use App\Repository\RoomRepository;
+use App\Repository\UserRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class MessageController extends AbstractController
 {
-    #[Route('/message/all/get/{roomId}', name: 'app_messages_get_for_room', methods: 'GET')]
+    #[Route('/message/all/get/{roomId}', name: 'app_message_get_for_room', methods: 'GET')]
     public function getNewsMessages(int $roomId,
                                     SerializerInterface $serializer,
                                     RoomRepository $roomRepository): JsonResponse 
@@ -25,8 +30,42 @@ class MessageController extends AbstractController
             'messages' => json_decode(
                 $serializer->serialize(
                     $room->getMessages(),
-                    'json', ['depth' => 1]
+                    'json', ['groups' => ['room']]
             ))
+        ]);
+    }
+
+    #[Route('/message/new', name: 'app_message_new_for_room', methods: 'POST')]
+    public function addNewsMessage(Request $request,
+                                   EntityManagerInterface $entityManager,
+                                   SerializerInterface $serializer,
+                                   UserRepository $userRepository,
+                                   RoomRepository $roomRepository): JsonResponse 
+    {         
+        $data = json_decode($request->getContent(), true);
+        $room = $roomRepository->findOneByIdField($data['roomId']);
+        $user = $userRepository->findOneByUsernameField($data['username']);
+        if (!$room || !$user) {
+            throw new HttpException(422, 'Не удалось осуществить операцию');
+        } 
+
+        $message = new Message();
+        $message->setSender($user);
+        $room->addMessage($message);
+        $currentTime = new DateTimeImmutable();
+        $message->setDispatchTime($currentTime);
+        $message->setInformation($data['information']);
+
+        $entityManager->persist($message);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Ok',
+            'main' => 'Успешно отправлено.',            
+            'holding' => false,
+            'message' => json_decode(
+                $serializer->serialize(
+                $message, 'json', ['groups' => ['room']]
+            )) 
         ]);
     }
 }
