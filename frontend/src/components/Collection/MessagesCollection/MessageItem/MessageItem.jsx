0c19ef1207e30f457e3134a2d37477d61203ff60
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-24 18:43:10 +0300

    Обновленная улучшенная реализация и стиль элемента сообщения.

--- frontend/src/components/Collection/MessagesCollection/MessageItem/MessageItem.jsx
+++ frontend/src/components/Collection/MessagesCollection/MessageItem/MessageItem.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import classes from './MessageItem.module.css'
+import { useUserContext } from '../../../../contexts/UserContext/UserProvider'
 
 export default function MessageItem({message, ...props}) {
+    const { user } = useUserContext();
+    
     return (
         <div className={classes.MessageItem} {...props}>
-            <p>{message.information}</p>
-            <p>{message.information}</p>
+            <div>
+                <p>{message.information}</p>
+            </div>
+            <div>                
+                <h6>
+                    {user.id !== message.sender.id 
+                        ? " Это новость другого пользователя"
+                        : ''} 
+                    {message.dispatch_time}
+                    {user.id === message.sender.id 
+                        ? " Это ваша новость"
+                        : ''}
+                </h6> 
+            </div>   
         </div>
     )
 }
