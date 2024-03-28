7cdf95d894ef3aeeedd9adc4a7d72a078391e0cc
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-03-28 18:04:00 +0300

    Новая функция сохранения информации о текущем основном блоке.

--- frontend/src/contexts/CookieContext.js
+++ frontend/src/contexts/CookieContext.js
@@ -1,3 +1,7 @@
 import Cookies from 'universal-cookie';
 
 export const cookies = new Cookies(null, { path: '/' });
+
+export const saveLastMainBlock = function(mainBlock) {
+    cookies.set('lastMainBlock', mainBlock);
+} 
