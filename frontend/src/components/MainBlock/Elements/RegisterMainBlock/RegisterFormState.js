43310017ae852ab3f9e18773939161bcd51b4801
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-07 18:37:51 +0300

    Разделение полной проверки по отдельным функциям для отслеживания подсказок.

--- frontend/src/components/MainBlock/Elements/RegisterMainBlock/RegisterFormState.js
+++ frontend/src/components/MainBlock/Elements/RegisterMainBlock/RegisterFormState.js
@@ -9,27 +9,40 @@ const formName = 'registerForm'
 
 export function validRegisterForm(setTips) {
     let usernameIsOk = !formParamIsEmpty(formName, 'username');
-    let emailIsOk = !formParamIsEmpty(formName, 'email') && 
-        formEmailIsCorrect(
-            formName,
-            'email',
-            "Неверный формат почты",
-            setTips
-        );
+    let emailIsOk = !formParamIsEmpty(formName, 'email') &&
+                     validEmail(setTips);
     let passwordIsOk = !formParamIsEmpty(formName, 'password') &&
-        !formParamIsSmall(
-            formName,
-            'password',
-            "Пароль слишком короткий",
-            setTips
-        );
+                        validPassword(setTips);        
     let passwordAgainIsOk = !formParamIsEmpty(formName, 'passwordAgain') &&
-        passwordIsRepeated(
-            formName,
-            'password',
-            'passwordAgain',
-            "Пароли не совпадают",
-            setTips
-        );
+                             validPasswordAgain(setTips);        
     return usernameIsOk && emailIsOk && passwordIsOk && passwordAgainIsOk;     
 }
+
+export function validEmail(setTips) {
+    return !formEmailIsCorrect(
+        'registerForm',
+        'email',
+        "Неверный формат почты",
+        setTips
+    );
+}
+
+export function validPassword(setTips) {
+    return !formParamIsSmall(
+        formName,
+        'password',
+        "Пароль слишком короткий",
+        setTips
+    );
+}
+
+export function validPasswordAgain(setTips) {
+    return passwordIsRepeated(
+        formName,
+        'password',
+        'passwordAgain',
+        "Пароли не совпадают",
+        setTips
+    );
+}
+
