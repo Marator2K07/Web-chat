ec1595f03a53861eb1f8139dc91b45574c723d07
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-07 18:22:20 +0300

    Максимальное расширение проверки состояния формы регистрации.

--- frontend/src/components/MainBlock/Elements/RegisterMainBlock/RegisterFormState.js
+++ frontend/src/components/MainBlock/Elements/RegisterMainBlock/RegisterFormState.js
@@ -1,11 +1,35 @@
-import { formParamIsEmpty } from "../../../../utils"
+import {
+    formEmailIsCorrect,
+    formParamIsEmpty,
+    formParamIsSmall,
+    passwordIsRepeated
+} from "../../../../utils"
 
 const formName = 'registerForm'
 
-export function checkRegisterForm() {
+export function validRegisterForm(setTips) {
     let usernameIsOk = !formParamIsEmpty(formName, 'username');
-    let emailIsOk = !formParamIsEmpty(formName, 'email');
-    let passwordIsOk = !formParamIsEmpty(formName, 'password');
-    let passwordAgainIsOk = !formParamIsEmpty(formName, 'passwordAgain');
+    let emailIsOk = !formParamIsEmpty(formName, 'email') && 
+        formEmailIsCorrect(
+            formName,
+            'email',
+            "Неверный формат почты",
+            setTips
+        );
+    let passwordIsOk = !formParamIsEmpty(formName, 'password') &&
+        !formParamIsSmall(
+            formName,
+            'password',
+            "Пароль слишком короткий",
+            setTips
+        );
+    let passwordAgainIsOk = !formParamIsEmpty(formName, 'passwordAgain') &&
+        passwordIsRepeated(
+            formName,
+            'password',
+            'passwordAgain',
+            "Пароли не совпадают",
+            setTips
+        );
     return usernameIsOk && emailIsOk && passwordIsOk && passwordAgainIsOk;     
 }
