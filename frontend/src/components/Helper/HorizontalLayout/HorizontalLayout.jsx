fa2d9265658287a0ab7be2f7dea88d3f12f0ecb7
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-08 16:08:54 +0300

    Создан новый компонент-помощник для корректного отображения группы.

---
+++ frontend/src/components/Helper/HorizontalLayout/HorizontalLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './HorizontalLayout.module.css'
+
+export default function HorizontalLayout({...props}) {
+  return (
+    <div className={classes.HorizontalLayout} {...props}>
+        
+    </div>
+  )
+}
