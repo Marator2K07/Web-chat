db411267da0aea9d4195796f6acf652b5e4cad3a
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-11 17:27:42 +0300

    Теперь не нужно сохранять вручную куки навигации.
    как следствие, убрана лишняя функция из контекста куки.

--- frontend/src/components/Collection/NavigationCollection/NavigationItem/NavigationItem.jsx
+++ frontend/src/components/Collection/NavigationCollection/NavigationItem/NavigationItem.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import classes from './NavigationItem.module.css'
 import { useNavigationContext } from '../../../../contexts/NavigationContext/NavigationProvider';
-import { saveLastMainBlock } from '../../../../contexts/CookieContext';
 
 export default function NavigationItem({block, ...props}) {
-    const {goNavigationWithAnimation} = useNavigationContext(); 
+    const { goNavigationWithAnimation } = useNavigationContext(); 
 
     return (
         <div className={classes.NavigationItem} {...props}>
-            <button onClick={() => {
-                saveLastMainBlock(block);
-                goNavigationWithAnimation(block);                
-                }}>
+            <button onClick={() => goNavigationWithAnimation(block)}>
                 {block.description}
             </button>            
         </div>
--- frontend/src/contexts/CookieContext.js
+++ frontend/src/contexts/CookieContext.js
@@ -2,6 +2,3 @@ import Cookies from 'universal-cookie';
 
 export const cookies = new Cookies(null, { path: '/' });
 
-export const saveLastMainBlock = function(mainBlock) {
-    cookies.set('lastMainBlock', mainBlock);
-} 
