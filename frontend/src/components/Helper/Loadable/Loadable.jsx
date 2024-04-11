09062d935a6024f5f9a78035150471db93a02102
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-11 15:02:40 +0300

    Создан новый компонент - помощник. Пока начальная реализация + пустой стиль.

---
+++ frontend/src/components/Helper/Loadable/Loadable.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Loadable.module.css'
+
+export default function Loadable() {
+  return (
+    <div className={classes.Loadable} {...props}>
+
+    </div>
+  )
+}
