9f02560971154db623c675e79bef06729422d86f
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-12 07:29:17 +0300

    Управление загрузкой нужного блока в зависимости от страницы вынесено в функцию.

--- frontend/src/contexts/CookieContext.js
+++ frontend/src/contexts/CookieContext.js
@@ -2,3 +2,18 @@ import Cookies from 'universal-cookie';
 
 export const cookies = new Cookies(null, { path: '/' });
 
+export const loadLastMainBlock = function(currentPage) {
+    let result = cookies.get(currentPage);
+    if (result) {
+        return result;
+    }
+
+    switch (currentPage) {
+        case 'lastBeforeLoginBlock':
+            return "loginBlock"
+        case 'lastAfterLoginBlock':
+            return "welcomeBlock"
+        default:
+            break;
+    }
+}
