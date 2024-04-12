424f258c799641606efa24184b241ac79b8eab85
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-12 11:16:53 +0300

    Изменение отступа.

--- frontend/src/components/Helper/HorizontalLayout/HorizontalLayout.jsx
+++ frontend/src/components/Helper/HorizontalLayout/HorizontalLayout.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import classes from './HorizontalLayout.module.css'
 
 export default function HorizontalLayout({...props}) {
-  return (
-    <div className={classes.HorizontalLayout} {...props}>
+    return (
+        <div className={classes.HorizontalLayout} {...props}>
         
-    </div>
-  )
+        </div>
+    )
 }
