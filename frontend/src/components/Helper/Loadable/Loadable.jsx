ec4b22aa47b311679784cb52eec62611b1345200
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-15 11:57:34 +0300

    Следующая тестовая реализация компонента помощника загрузчика.

--- frontend/src/components/Helper/Loadable/Loadable.jsx
+++ frontend/src/components/Helper/Loadable/Loadable.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Loadable.module.css'
+import { Spin } from 'antd';
+import { SyncOutlined } from '@ant-design/icons';
+import { LOADING_INDICATOR_COLOR, LOADING_INDICATOR_SIZE } from '../../../constants';
 
-export default function Loadable() {
-  return (
-    <div className={classes.Loadable} {...props}>
-
-    </div>
-  )
+export default function Loadable({...props}) {
+    const [isReady, setReady] = useState(false);
+    const [error, setError] = useState(null);
+    
+    return (
+        <div className={classes.Loadable} {...props}>
+            {
+                !isReady &&
+                <Spin
+                    size='large'
+                    indicator={
+                        <SyncOutlined spin style={{
+                            fontSize: LOADING_INDICATOR_SIZE,
+                            color: LOADING_INDICATOR_COLOR
+                        }} />
+                    } />                
+            } 
+            {
+                error &&
+                <h4>error.response.data.detail</h4>
+            }
+        </div>
+    )        
 }
