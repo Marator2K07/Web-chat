5b58ac65075e12514235c4b3b67bd586ac5fb363
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-15 14:26:26 +0300

    Усовершенствованная реализация с обработкой отсутствия данных.

--- frontend/src/components/View/AboutUserView.jsx
+++ frontend/src/components/View/AboutUserView.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
 import classes from './AboutUserView.module.css'
+import Loadable from '../Helper/Loadable/Loadable'
+import { useUserContext } from '../../contexts/UserContext/UserProvider';
+import { useLocation } from 'react-router-dom';
 
-export default function AboutUserView({ aboutUser, ...props }) {
-    return (
-        <div className={classes.AboutUserView} {...props}>
-            <h4>Фото аккаунта:</h4>
-            {!aboutUser.image ? <img
-                                  src={`${window.location.origin}/DefUserIcon.png`}
-                                  alt="Not found"/>
-                              : <img 
-                                  src={aboutUser.image}
-                                  alt="Not found"/>}
-            <h4>Имя:</h4>
-            <p>{aboutUser.name}</p>
-            <h4>Фамилия:</h4>
-            <p>
-                {aboutUser.secondname ? aboutUser.secondname
-                                      : 'Не задано'}
-            </p>
-            <h4>День рождения:</h4>
-            <p>
-                {aboutUser.dateOfBirth ? aboutUser.dateOfBirth
-                                       : 'Не задано'}
-            </p>
-        </div>
-    )
+export default function AboutUserView({...props}) {
+    const { aboutUser, loadAboutUser } = useUserContext();
+    const location = useLocation();
+
+    if (aboutUser) {
+        return (
+            <div className={classes.AboutUserView} {...props}>
+                <h4>Фото аккаунта:</h4>
+                {!aboutUser.image ? <img
+                                      src={`${window.location.origin}/DefUserIcon.png`}
+                                      alt="Not found" />
+                                  : <img 
+                                      src={aboutUser.image}
+                                      alt="Not found" />}
+                <h4>Имя:</h4>
+                <p>{aboutUser.name}</p>
+                <h4>Фамилия:</h4>
+                <p>
+                    {aboutUser.secondname ? aboutUser.secondname
+                                          : 'Не задано'}
+                </p>
+                <h4>День рождения:</h4>
+                <p>
+                    {aboutUser.dateOfBirth ? aboutUser.dateOfBirth
+                                           : 'Не задано'}
+                </p>
+            </div>
+        )
+    } else {
+        return (
+            <Loadable
+                propertyName={'aboutUser'}
+                getDataUrl={`${location.pathname}/about`}
+                setDataFunc={loadAboutUser} />
+        )
+    }    
 }
