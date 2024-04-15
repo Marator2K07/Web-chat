7e4fa8ee981e7a94a4f25cb937e108457ef8a4b0
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-15 14:30:21 +0300

    Подправлен стиль.

--- frontend/src/components/MainBlock/Elements/LoginMainBlock/LoginMainBlock.jsx
+++ frontend/src/components/MainBlock/Elements/LoginMainBlock/LoginMainBlock.jsx
@@ -72,21 +72,23 @@ export default function LoginMainBlock({...props}) {
     return (
         <div className={classes.LoginMainBlock} {...props}>
             <Scrollable>
-            <form name='loginForm'>
-                <h4>Введите имя аккаунта:</h4>
-                <input
-                    type='username'
-                    name='username'
-                    value={credentials.username}
-                    onChange={handleChange}/>
-                <h4>А теперь пароль:</h4>    
-                <input
-                    type='password'
-                    name='password'
-                    value={credentials.password}                     
-                    onChange={handleChange}/>
-                <button type='button' onClick={handleSubmit}>Войти</button>
-            </form> 
+                <form name='loginForm'>
+                    <h4>Введите имя аккаунта:</h4>
+                    <input
+                        type='username'
+                        name='username'
+                        value={credentials.username}
+                        onChange={handleChange}/>
+                    <h4>А теперь пароль:</h4>    
+                    <input
+                        type='password'
+                        name='password'
+                        value={credentials.password}                     
+                        onChange={handleChange}/>
+                    <button type='button' onClick={handleSubmit}>
+                        Войти
+                    </button>
+                </form> 
             </Scrollable>                
         </div>
     )
