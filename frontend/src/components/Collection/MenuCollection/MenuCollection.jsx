978a06302873cff307d81156b18f99da7615d5dd
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-20 19:59:14 +0300

    Тестовая реализация с первым вынесенным объектом меню.

--- frontend/src/components/Collection/MenuCollection/MenuCollection.jsx
+++ frontend/src/components/Collection/MenuCollection/MenuCollection.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import classes from './MenuCollection.module.css'
+import { useLoadingContext } from '../../../contexts/LoadingContext/LoadingProvider';
+import { useResponseHandlerContext } from '../../../contexts/ResponseHandlerContext/ResponseHandlerProvider';
+import { useNavigate } from 'react-router-dom';
+import { LOGOUT_ROUTE } from '../../../constants';
+import { cookies, removeUserCookies } from '../../../contexts/CookieContext';
 
-export default function MenuCollection({...props}) {
+export default function MenuCollection({items, ...props}) {    
+    const { startLoading, stopLoading } = useLoadingContext();
+    const { resetResult, makePostRequest } = useResponseHandlerContext();
+    const navigate = useNavigate();
+
+    const items = {
+        logout: {
+            info: "Выйти из аккаунта",
+            url: "/",
+            action: async() => {
+                startLoading();
+                resetResult();
+                await makePostRequest(
+                    LOGOUT_ROUTE,
+                    { refreshToken: cookies.get('refreshToken') },
+                    () => {
+                        removeUserCookies();
+                        if (url)
+                            navigate(url, { replace: true });
+                    }
+                );
+                stopLoading();
+            }
+        }
+    }
+
     return (
         <div className={classes.MenuCollection} {...props}>
-            
+
         </div>
     )
 }
