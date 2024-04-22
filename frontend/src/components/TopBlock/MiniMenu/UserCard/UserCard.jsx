9a8113cc8dc2f7ede4efc868fad20a7a909465b9
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-22 10:07:04 +0300

    Убраны лишние свойства у блоков + подправлен стиль.

--- frontend/src/components/TopBlock/MiniMenu/UserCard/UserCard.jsx
+++ frontend/src/components/TopBlock/MiniMenu/UserCard/UserCard.jsx
@@ -4,23 +4,29 @@ import { useUserContext } from '../../../../contexts/UserContext/UserProvider'
 
 export default function UserCard({...props}) {
     const { aboutUser } = useUserContext();
+
     // в случае если пользователь не установил картинку - ставим
     // картинку по умолчанию из файлов проекта
-    const userImg = !aboutUser || !aboutUser.image ?
-                    `${window.location.origin}/DefUserIcon.png` :
-                    aboutUser.image
+    const userImg = !aboutUser || !aboutUser.image
+        ? `${window.location.origin}/DefUserIcon.png`
+        : aboutUser.image
 
     return (
         <div className={classes.UserCard} {...props}> 
-            <div className='picture'>
+            <div>
                 <img src={userImg} alt="" />
             </div>           
-            <div className='info'>
+            <div>
                 <p>
-                    { aboutUser ? `${aboutUser.name}` : 'Не авторизовано'}                        
+                    {aboutUser
+                        ? `${aboutUser.name}`
+                        : 'Не авторизовано'}                        
                 </p>
                 <p>
-                    { aboutUser ? `${aboutUser.secondname ? aboutUser.secondname : 'без фамилии'}` 
+                    {aboutUser
+                        ? `${aboutUser.secondname
+                            ? aboutUser.secondname
+                            : 'без фамилии'}` 
                         : ''}                        
                 </p>
             </div>            
