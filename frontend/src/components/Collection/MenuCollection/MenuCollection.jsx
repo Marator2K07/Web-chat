c6746a41e83e720ed24a54abcb7d91fc215b2d03
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-22 17:54:55 +0300

    Предположительная реализация компонента коллекции пунктов меню.
    + исправленный стиль.

--- frontend/src/components/Collection/MenuCollection/MenuCollection.jsx
+++ frontend/src/components/Collection/MenuCollection/MenuCollection.jsx
@@ -1,40 +1,13 @@
 import React from 'react'
 import classes from './MenuCollection.module.css'
-import { useLoadingContext } from '../../../contexts/LoadingContext/LoadingProvider';
-import { useResponseHandlerContext } from '../../../contexts/ResponseHandlerContext/ResponseHandlerProvider';
-import { useNavigate } from 'react-router-dom';
-import { LOGOUT_ROUTE } from '../../../constants';
-import { cookies, removeUserCookies } from '../../../contexts/CookieContext';
+import MenuItem from './MenuItem/MenuItem'
 
-export default function MenuCollection({items, ...props}) {    
-    const { startLoading, stopLoading } = useLoadingContext();
-    const { resetResult, makePostRequest } = useResponseHandlerContext();
-    const navigate = useNavigate();
-
-    const items = {
-        logout: {
-            info: "Выйти из аккаунта",
-            url: "/",
-            action: async() => {
-                startLoading();
-                resetResult();
-                await makePostRequest(
-                    LOGOUT_ROUTE,
-                    { refreshToken: cookies.get('refreshToken') },
-                    () => {
-                        removeUserCookies();
-                        if (url)
-                            navigate(url, { replace: true });
-                    }
-                );
-                stopLoading();
-            }
-        }
-    }
-
+export default function MenuCollection({items, ...props}) { 
     return (
         <div className={classes.MenuCollection} {...props}>
-
+            {Object.keys(items).map((key) => (
+                <MenuItem item={items[key]}/>
+            ))}
         </div>
     )
 }
