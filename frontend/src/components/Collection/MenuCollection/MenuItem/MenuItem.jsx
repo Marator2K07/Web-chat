f48aac018f7d5b30e9a3f1dbe7199f049f1444c7
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-22 18:16:09 +0300

    Исправление ошибок в реализации и написание стиля с учетом глобального.
    + расширение глобального стиля.

--- frontend/src/components/Collection/MenuCollection/MenuItem/MenuItem.jsx
+++ frontend/src/components/Collection/MenuCollection/MenuItem/MenuItem.jsx
@@ -3,9 +3,9 @@ import classes from './MenuItem.module.css'
 
 export default function MenuItem({item, ...props}) {
     return (
-        <div className={classes.UserItem} {...props}>
+        <div className={classes.MenuItem} {...props}>
             <button type='button' onClick={item.action}>
-                {item.text}
+                {item.info}
             </button>
         </div>
     )
