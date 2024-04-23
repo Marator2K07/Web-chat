a559b5a592f50ce8a167297088346ef8b392a545
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-23 17:18:42 +0300

    Создан новый раздел для форм куда добавлен первый компонент.
    Со стартовым кодом и пустым стилем.

---
+++ frontend/src/components/Form/LoginForm/LoginForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './LoginForm.module.css'
+
+export default function LoginForm({...props}) {
+    return (
+        <div className={classes.LoginForm} {...props}>
+            
+        </div>
+    )
+}
