c77bdb1d723cca59e2ea8806c78d3d5d2be11fa3
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-26 17:21:31 +0300

    Создан новый компонент с начальной реализацией и пустым стилем.

---
+++ frontend/src/components/View/RoomsView/RoomsView.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './RoomsView.module.css'
+
+export default function RoomsView({...props}) {
+    return (
+        <div className={classes.RoomsView} {...props}>
+            
+        </div>
+    )
+}
