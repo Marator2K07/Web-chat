b88a121fe9dfbdc5111f219ec2b00c9a13602d3f
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-28 11:32:01 +0300

    Написана предполагаемая реализация для компонента.

--- frontend/src/components/View/RoomsView/RoomsView.jsx
+++ frontend/src/components/View/RoomsView/RoomsView.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import classes from './RoomsView.module.css'
+import { useUserContext } from '../../../contexts/UserContext/UserProvider'
+import { useLocation } from 'react-router-dom';
+import RoomsCollection from '../../Collection/RoomsCollection/RoomsCollection';
+import Loadable from '../../Helper/Loadable/Loadable';
+import { cookies } from '../../../contexts/CookieContext';
 
 export default function RoomsView({...props}) {
-    return (
-        <div className={classes.RoomsView} {...props}>
-            
-        </div>
-    )
+    const { rooms, loadRooms } = useUserContext();
+    const location = useLocation();
+
+    if (rooms) {
+        return (
+            <div className={classes.RoomsView} {...props}>
+                <RoomsCollection items={rooms} />
+            </div>
+        )
+    } else {
+        return (
+            <Loadable
+                isWorking={cookies.get('username')}
+                propertyName={'rooms'}
+                getDataUrl={`${location.pathname}/rooms`}
+                setDataFunc={loadRooms} /> 
+        )
+    }
 }
