339d0d95e34eef69b3fdabe3e28c96339d89edb7
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-04-29 19:08:24 +0300

    Создан новый компонент с пока недоделанной реализацией.
     + пустым стилем.

---
+++ frontend/src/components/View/UsersView/UsersView.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from './UsersView.module.css'
+import Loadable from '../../Helper/Loadable/Loadable'
+
+export default function UsersView({users, loading, ...props}) {
+
+    if (users) {
+        return (
+            <div className={classes.UsersView} {...props}>
+                
+            </div>
+        )
+    } else {
+        <Loadable 
+            isWorking={loading}
+             />
+    }
+    
+}
