1d7f01841dcfc442b332aca1c2157a10cd502e71
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-01 12:05:55 +0300

    Создан компонент представления найденного пользователя.
    + добавлен пустой стиль.

---
+++ frontend/src/components/View/UserItemView/UserItemView.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import classes from './UserItemView.module.css'
+import Loadable from '../../Helper/Loadable/Loadable';
+import { cookies } from '../../../contexts/CookieContext';
+import { ANOTHER_USER_PAGE_URL, GET_ABOUT_USER_ROUTE_END } from '../../../constants';
+import UserItem from '../../Collection/UsersCollection/UserItem/UserItem';
+
+export default function UserItemView({user, button, ...props}) {
+    const [aboutUser, loadAboutUser] = useState(null);
+
+    if (aboutUser) {
+        return (
+            <div className={classes.AboutUserView} {...props}>
+                <UserItem 
+                    aboutUser={aboutUser} 
+                    button={button} />
+            </div>
+        )
+    } else {
+        return (
+            <Loadable 
+                isWorking={cookies.get('username')}
+                propertyName={'aboutUser'}
+                getDataUrl={`${ANOTHER_USER_PAGE_URL}
+                            /${user.username}
+                            /${GET_ABOUT_USER_ROUTE_END}`
+                        }
+                setDataFunc={loadAboutUser} />
+        )
+    }
+
+    
+}
