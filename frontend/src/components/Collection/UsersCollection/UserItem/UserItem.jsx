15fbeab82d58162d6f7d30b142489a8f5246ff3c
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-01 12:03:41 +0300

    Усовершенствование реализации + временное упрощение.
    (пока не рассматриваем возможность навигации на страницу другого пользователя)

--- frontend/src/components/Collection/UsersCollection/UserItem/UserItem.jsx
+++ frontend/src/components/Collection/UsersCollection/UserItem/UserItem.jsx
@@ -1,33 +1,29 @@
 import React from 'react'
 import classes from './UserItem.module.css'
 
-export default function UserItem({user,
-                                  buttonName,
-                                  buttonHandler,
-                                  navigateHandler,
+export default function UserItem({aboutUser,
+                                  button,
                                   ...props}) {
     // в случае если пользователь не установил картинку - ставим
     // картинку по умолчанию из файлов проекта
-    const userImg = !user.aboutUser.image ?
-                    `${window.location.origin}/DefUserIcon.png` :
-                    user.aboutUser.image
+    const userImg = !aboutUser.image
+                ? `${window.location.origin}/DefUserIcon.png`
+                :  aboutUser.image
 
     return (
-        <div 
-            className={classes.UserItem}
-            onClick={() => navigateHandler(user.username)} {...props}>
+        <div className={classes.UserItem} {...props}>
             <img src={userImg} alt="Not found" />
             <div>
-                <p>{`${user.aboutUser.name}`}</p>
-                <p>{`${user.aboutUser.secondname}`}</p>
+                <p>{aboutUser.name}</p>
+                <p>{aboutUser.secondname}</p>
             </div>
             {
-                buttonName &&
+                button &&
                 <button type="button" onClick={(e) =>{
                     e.stopPropagation(); // событие нажатия дальше не пойдет
-                    buttonHandler(user)
+                    button.action(user);
                 }}>
-                    {buttonName}
+                    {button.name}
                 </button>
             }            
         </div>
