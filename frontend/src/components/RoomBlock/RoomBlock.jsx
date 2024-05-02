89f019c44c242d5057c360fc69446bb6ddcfba74
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-02 08:41:21 +0300

    Компонент блока комнаты теперь прокручиваемый.
    + мелкие обновления в стилях.

--- frontend/src/components/RoomBlock/RoomBlock.jsx
+++ frontend/src/components/RoomBlock/RoomBlock.jsx
@@ -7,6 +7,7 @@ import { MEDIUM_DELAY, NEW_ROOM_ROUTE, USERS_SEARCH_ROUTE } from '../../constant
 import { useLoadingContext } from '../../contexts/LoadingContext/LoadingProvider';
 import RoomsView from '../View/RoomsView/RoomsView';
 import HorizontalLayout from '../Helper/HorizontalLayout/HorizontalLayout';
+import Scrollable from '../Helper/Scrollable/Scrollable';
 
 export default function RoomBlock({...props}) {
     const { toggleHolding, startLoading, stopLoading } = useLoadingContext();
@@ -102,46 +103,48 @@ export default function RoomBlock({...props}) {
 
     return (
         <div className={classes.RoomBlock} {...props}>
-            <h3> { !showRoomForm ? 'Текущие чаты'
-                                 : 'Создание нового чата'} 
-            </h3>
-            {
-                !showRoomForm ?
-                <RoomsView handleAction={handleAction} />
-                :
-                <form name='roomForm'>
-                    <h4>Название комнаты для общения:</h4>
-                    <input
-                        type='text'
-                        value={newRoomName.name}
-                        onChange={handleChange} />
-                    <h4>Участники:</h4>
-                    <UsersCollection
-                        users={selectedUsers}
-                        clue={'...Нет никого кроме вас...'}
-                        button={removeUserButton} />
-
-                    <h4>Поиск по имени и фамилии:</h4>
-                    <input
-                        type='text'
-                        value={searchStr}
-                        onChange={handleSearch} />
-                    <h4>Результаты поиска:</h4>
-                    <UsersCollection
-                        users={foundedUsers}                        
-                        clue={'...Никого нет...'}
-                        button={addUserButton} />
-
-                    <HorizontalLayout>
-                        <button type='button' onClick={handleSubmit}>
-                            Применить изменения
-                        </button>
-                        <button type='button' onClick={handleAction}>
-                            Назад
-                        </button>
-                    </HorizontalLayout>
-                </form>
-            }            
+            <Scrollable>            
+                <h3> { !showRoomForm ? 'Текущие чаты'
+                                     : 'Создание нового чата'} 
+                </h3>
+                {
+                    !showRoomForm ?
+                    <RoomsView handleAction={handleAction} />
+                    :
+                    <form name='roomForm'>
+                        <h4>Название комнаты для общения:</h4>
+                        <input
+                            type='text'
+                            value={newRoomName}
+                            onChange={handleChange} />
+                        <h4>Участники:</h4>
+                        <UsersCollection
+                            users={selectedUsers}
+                            clue={'...Нет никого кроме вас...'}
+                            button={removeUserButton} />
+    
+                        <h4>Поиск по имени и фамилии:</h4>
+                        <input
+                            type='text'
+                            value={searchStr}
+                            onChange={handleSearch} />
+                        <h4>Результаты поиска:</h4>
+                        <UsersCollection
+                            users={foundedUsers}                        
+                            clue={'...Никого нет...'}
+                            button={addUserButton} />
+    
+                        <HorizontalLayout>
+                            <button type='button' onClick={handleSubmit}>
+                                Применить изменения
+                            </button>
+                            <button type='button' onClick={handleAction}>
+                                Назад
+                            </button>
+                        </HorizontalLayout>
+                    </form>
+                }
+            </Scrollable>            
         </div>
     )
 }
