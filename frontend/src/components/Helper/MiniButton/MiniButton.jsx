3033436c620e524e560ba5ce4619c7b88a87ea0a
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-03 10:55:11 +0300

    Данный компонент теперь является адаптацией кнопки + дописан стиль.
    и связанное расширение файла констант.

--- frontend/src/components/Helper/MiniButton/MiniButton.jsx
+++ frontend/src/components/Helper/MiniButton/MiniButton.jsx
@@ -4,12 +4,12 @@ import classes from './MiniButton.module.css'
 export default function MiniButton({button, ...props}) {
     return (
         <div className={classes.MiniButton} {...props}>
-            <button type='button' onClick={(e) => {
+            <p onClick={(e) => {
                 e.stopPropagation(); // событие нажатия дальше не пойдет
                 button.action();
             }}>
                 {button.name}
-            </button>
+            </p> 
         </div>
     )
 }
