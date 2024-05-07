bc7413c16cce410caeda8fc7beb72cebf8bcbb4e
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-07 17:49:16 +0300

    Добавлен метод обработки удаления выбранной комнаты.
    + использование новой константы.

--- frontend/src/components/View/RoomsView/RoomsView.jsx
+++ frontend/src/components/View/RoomsView/RoomsView.jsx
@@ -5,10 +5,33 @@ import { useLocation } from 'react-router-dom';
 import RoomsCollection from '../../Collection/RoomsCollection/RoomsCollection';
 import Loadable from '../../Helper/Loadable/Loadable';
 import { cookies } from '../../../contexts/CookieContext';
+import { useLoadingContext } from '../../../contexts/LoadingContext/LoadingProvider';
+import { useResponseHandlerContext } from '../../../contexts/ResponseHandlerContext/ResponseHandlerProvider';
+import { DELETE_ROOM_ROUTE_END } from '../../../constants';
 
 export default function RoomsView({handleAction, ...props}) {
+    const { resetResult, makePostRequest } = useResponseHandlerContext();
+    const { startLoading, stopLoading } = useLoadingContext();    
     const { rooms, loadRooms } = useUserContext();
     const location = useLocation();
+
+    // запрос на удаление выбранной комнаты
+    const removeSelectedRoom = async (selectedRoom) => {
+        startLoading();
+        resetResult();
+        await makePostRequest(
+            location.pathname + DELETE_ROOM_ROUTE_END,
+            { room: selectedRoom },
+            () => {
+                // если все прошло успешно, то
+                // убираем комнату из текущей коллекции фронтэнда
+                const newRooms = rooms
+                    .filter(room => room.id !== selectedRoom.id);
+                loadRooms(newRooms);
+            }
+        )
+        stopLoading();
+    }
 
     if (rooms) {
         return (
