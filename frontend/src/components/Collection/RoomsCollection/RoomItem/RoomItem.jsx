b26881527c13e411ae78053b8370d70639fcc8bf
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-07 18:03:43 +0300

    В элементе комнаты теперь есть возможность обработки кнопки.

--- frontend/src/components/Collection/RoomsCollection/RoomItem/RoomItem.jsx
+++ frontend/src/components/Collection/RoomsCollection/RoomItem/RoomItem.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import classes from './RoomItem.module.css'
 import { useUserContext } from '../../../../contexts/UserContext/UserProvider';
+import MiniButton from '../../../Helper/MiniButton/MiniButton';
 
-export default function RoomItem({item, ...props}) {
+export default function RoomItem({item, button, ...props}) {
     const { loadCurrentRoom } = useUserContext();    
 
     return (
         <div
             className={classes.RoomItem}
             onClick={() => {loadCurrentRoom(item)}} {...props}>
-            <p>{item.name}</p>
+            <div>
+                <p>{item.name}</p>
+            </div>
+            {
+                button &&
+                <MiniButton
+                    button={button}
+                    data={item} />
+            }
         </div>
     )
 }
