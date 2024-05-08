70a59eccf58423d28df717a2b82f0c14f4800692
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-08 15:25:04 +0300

    Добавлен будущий компонент для формы принятия действия.
    пока с начальной реализацией и пустым стилем.

---
+++ frontend/src/components/Form/AcceptActionForm/AcceptActionForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './AcceptActionForm.module.css'
+
+export default function AcceptActionForm({...props}) {
+    return (
+        <div className={classes.AcceptActionForm} {...props}>
+            AcceptActionForm
+        </div>
+    )
+}
