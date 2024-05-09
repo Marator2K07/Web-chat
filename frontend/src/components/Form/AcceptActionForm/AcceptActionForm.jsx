b5d05ea8366abd82b869934253dbdf0880b92143
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-09 19:58:09 +0300

    Дописана предположительная реализация.
    + связанное расширение файла констант.

--- frontend/src/components/Form/AcceptActionForm/AcceptActionForm.jsx
+++ frontend/src/components/Form/AcceptActionForm/AcceptActionForm.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import classes from './AcceptActionForm.module.css'
+import { ACCEPT_ACTION_FORM_NAME } from '../../../constants'
+import HorizontalLayout from '../../Helper/HorizontalLayout/HorizontalLayout'
 
-export default function AcceptActionForm({...props}) {
+export default function AcceptActionForm({handleSubmit,
+                                          handleCancel,
+                                          ...props}) {
     return (
         <div className={classes.AcceptActionForm} {...props}>
-            AcceptActionForm
+            <form name={ACCEPT_ACTION_FORM_NAME}>
+                <p>Подтверждение выполнения операции</p>
+                <HorizontalLayout>
+                    <button type='button' onClick={handleSubmit}>
+                        Подтвердить
+                    </button>
+                    <button type='button' onClick={handleCancel}>
+                        Отмена
+                    </button>
+                </HorizontalLayout> 
+            </form>
         </div>
     )
 }
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -46,6 +46,7 @@ export const DATE_FORMAT = "YYYY-MM-DD"
 export const LOGIN_FORM_NAME = "loginForm"; 
 export const REGISTRATION_FORM_NAME = "registrationForm";
 export const UPDATE_ABOUT_USER_FORM_NAME = "updateAboutUserForm";
+export const ACCEPT_ACTION_FORM_NAME = "acceptActionForm"; 
 export const NEW_ROOM_FORM_NAME = "newRoomForm";
 
 export const PINK_EBONY_COLOR = "#704949";
