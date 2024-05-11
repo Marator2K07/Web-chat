4d2f6e48e23a57660c43e8844a5dd157c67e35bb
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-11 11:23:58 +0300

    Обработка отмены действия перенесена в сам компонент формы.

--- frontend/src/components/Form/AcceptActionForm/AcceptActionForm.jsx
+++ frontend/src/components/Form/AcceptActionForm/AcceptActionForm.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import classes from './AcceptActionForm.module.css'
-import { ACCEPT_ACTION_FORM_NAME } from '../../../constants'
+import { ACCEPT_ACTION_FORM_NAME, SHORT_DELAY } from '../../../constants'
 import HorizontalLayout from '../../Helper/HorizontalLayout/HorizontalLayout'
+import { useLoadingContext } from '../../../contexts/LoadingContext/LoadingProvider';
 
-export default function AcceptActionForm({handleSubmit,
-                                          handleCancel,
-                                          ...props}) {
+export default function AcceptActionForm({handleSubmit, ...props}) {
+    const { loading, toggleHolding } = useLoadingContext();    
+
+    const handleCancel = () => {
+        if (!loading) {
+            toggleHolding(SHORT_DELAY);
+        }
+    }
+    
     return (
         <div className={classes.AcceptActionForm} {...props}>
             <form name={ACCEPT_ACTION_FORM_NAME}>
