5c0586499d7f8c2c82b2b57c67d4bb62c48a6519
Marator2K07 <dmitriyshaaa@gmail.com>
2024-09-27 22:35:27 +0300

    Полная переработка стиля и реализации компонента BadResponse.

--- frontend/src/components/Response/BadResponse/BadResponse.jsx
+++ frontend/src/components/Response/BadResponse/BadResponse.jsx
@@ -5,19 +5,36 @@ import { useLoadingContext } from '../../../contexts/LoadingContext/LoadingProvi
 export default function BadResponse({message, ...props}) {  
     const { toggleHolding } = useLoadingContext();
     
+    if (!message.main) {
+        return;
+    }
+
     return (
         <div className={classes.BadResponse} {...props}>
-            <div className='main'>
-                <p>Сообщение: <br/><span style={{color:'#665d09'}}>{message.main}</span></p> 
-            </div>            
-            <div className='addition'>
-                <p>Дополнительная информация: <br/><span style={{color:'#665d09'}}>{message.addition}</span></p> 
-            </div>
-
-            {message.hasOwnProperty("holding") &&
-            <button type="button" onClick={() => toggleHolding(false, 0)}>
-                Вернуться
-            </button>}
+            <div>
+                {
+                    message.main && 
+                    <div>
+                        <h4>Сообщение:</h4>
+                        <p>{message.main}</p>
+                    </div>
+                }
+                { 
+                    message.addition &&
+                    <div>
+                        <h4>Дополнительная информация:</h4>
+                        <p>{message.addition}</p>
+                    </div>
+                }                 
+            </div> 
+            {
+                message.hasOwnProperty("holding") &&
+                <button
+                    type="button"
+                    onClick={() => toggleHolding(false, 0)}>
+                    Вернуться
+                </button>
+            }
         </div>        
     )
 }
