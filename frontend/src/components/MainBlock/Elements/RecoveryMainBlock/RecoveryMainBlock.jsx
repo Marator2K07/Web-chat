e71009dedba14f8eb5add3acc52c12deb1956ecb
Marator2K07 <dmitriyshaaa@gmail.com>
2024-09-30 21:31:36 +0300

    Добавлен новый компонент - блок восстановления.
    с начальной реализацией и пустым стилем.

---
+++ frontend/src/components/MainBlock/Elements/RecoveryMainBlock/RecoveryMainBlock.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './RecoveryMainBlock.module.css'
+
+export default function RecoveryMainBlock({...props}) {
+
+  return (
+    <div className={classes.RecoveryMainBlock} {...props}>
+        RecoveryMainBlock
+    </div>
+  )
+}
