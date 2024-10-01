fd381c73fe9b49d5c0162f19f195df3d44453987
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-01 20:53:06 +0300

    Добавление всего необходимого в форму начала восстановления.
    но метод обработки формы пока не написан.

--- frontend/src/components/MainBlock/Elements/StartRecoveryMainBlock/StartRecoveryMainBlock.jsx
+++ frontend/src/components/MainBlock/Elements/StartRecoveryMainBlock/StartRecoveryMainBlock.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './StartRecoveryMainBlock.module.css'
 import Scrollable from '../../../Helper/Scrollable/Scrollable'
 import StartRecoveryForm from '../../../Form/StartRecoveryForm/StartRecoveryForm'
+import TipsCollection from '../../../Collection/TipsCollection/TipsCollection';
+import { EXTRA_SHORT_DELAY } from '../../../../constants';
+import { validEmail } from './StartRecoveryFormState';
 
 export default function StartRecoveryMainBlock({...props}) {
+    const [tips, setTips] = useState({}); // подсказки для пользователя
+
     // данные формы
     const [recoveryInfo, setRecoveryInfo] = useState({
         email: ''
@@ -17,13 +22,27 @@ export default function StartRecoveryMainBlock({...props}) {
         });
     }  
 
+    // проверка спустя паузу корректности ввода емайла
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            recoveryInfo.email !== "" && validEmail(setTips);       
+        }, EXTRA_SHORT_DELAY);
+        return () => clearTimeout(timeout)
+    }, [recoveryInfo.email])
+
+    // обработка формы
+    async function handleSubmit(params) {
+        
+    }
+
     return (
         <div className={classes.StartRecoveryMainBlock} {...props}>
             <Scrollable>
                 <StartRecoveryForm
                     formData={recoveryInfo}
-                    handleChange={handleChange} />
-
+                    handleChange={handleChange} 
+                    handleSubmit={handleSubmit} />
+                <TipsCollection tips={tips}/>
             </Scrollable>            
         </div>
     )
