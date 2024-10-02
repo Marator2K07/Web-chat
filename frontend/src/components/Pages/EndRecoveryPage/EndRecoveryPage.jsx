1ca65192c27039da7b0ff0ea356d246275dcd13d
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-02 18:40:59 +0300

    Добавлена новая страница для окончания восстановления аккаунта.
    начальная реализация без стиля.

---
+++ frontend/src/components/Pages/EndRecoveryPage/EndRecoveryPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './EndRecoveryPage.module.css'
+
+export default function EndRecoveryPage({...props}) {
+    return (
+        <div className={classes.EndRecoveryPage} {...props}>
+            <h3>Восстановление аккаунта</h3>
+        </div>
+    )
+}
