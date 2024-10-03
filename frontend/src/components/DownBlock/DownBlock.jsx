9c024a095b16b6215f50416eaa19e082ca9060a4
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-03 12:06:10 +0300

    Исправили отступ и уменьшили хардкод в стиле.

--- frontend/src/components/DownBlock/DownBlock.jsx
+++ frontend/src/components/DownBlock/DownBlock.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import classes from './DownBlock.module.css'
 
 export default function DownBlock({...props}) {
-  return (
-    <div className={classes.DownBlock} {...props}>
-        <div className='left'>
-            <p>© 2024 Copyright: AwesomeChat.test</p>
+    return (
+        <div className={classes.DownBlock} {...props}>
+            <div className='left'>
+                <p>© 2024 Copyright: AwesomeChat.test</p>
+            </div>
+            <div className='right'>
+                Test
+            </div>
         </div>
-        <div className='right'>
-            Test
-        </div>
-    </div>
-  )
+    ) 
 }
