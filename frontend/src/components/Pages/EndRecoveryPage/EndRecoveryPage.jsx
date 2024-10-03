f578e87a53c7b0dc3027264adc0484239abb2449
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-03 12:33:25 +0300

    Доработан стиль и содержание компонента страницы окончания восстановления.

--- frontend/src/components/Pages/EndRecoveryPage/EndRecoveryPage.jsx
+++ frontend/src/components/Pages/EndRecoveryPage/EndRecoveryPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import classes from './EndRecoveryPage.module.css'
+import EndRecoveryMainBlock from '../../MainBlock/Elements/EndRecoveryMainBlock/EndRecoveryMainBlock';
+import DownBlock from '../../DownBlock/DownBlock';
+import Spacer from '../../Helper/Spacer/Spacer';
 
 export default function EndRecoveryPage({...props}) {
+    
     return (
         <div className={classes.EndRecoveryPage} {...props}>
+            <Spacer sizeW='10px' sizeH='13%'/>
             <h3>Восстановление аккаунта</h3>
+            <EndRecoveryMainBlock />
+            <Spacer sizeW='10px' sizeH='13%'/>
+            <DownBlock />
         </div>
     )
 }
