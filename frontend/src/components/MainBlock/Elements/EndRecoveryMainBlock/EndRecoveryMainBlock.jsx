bedc8aa59db026f1688fc3a0c5de8e7821fae132
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-04 09:58:28 +0300

    Дописан метод отправки формы для компонента завершения восстановления.

--- frontend/src/components/MainBlock/Elements/EndRecoveryMainBlock/EndRecoveryMainBlock.jsx
+++ frontend/src/components/MainBlock/Elements/EndRecoveryMainBlock/EndRecoveryMainBlock.jsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import classes from './EndRecoveryMainBlock.module.css'
+import bcrypt from 'bcryptjs-react';
 import Scrollable from '../../../Helper/Scrollable/Scrollable'
 import EndRecoveryForm from '../../../Form/EndRecoveryForm/EndRecoveryForm'
 import TipsCollection from '../../../Collection/TipsCollection/TipsCollection';
-import { EXTRA_SHORT_DELAY } from '../../../../constants';
+import { END_RECOVERY_ROUTE, EXTRA_SHORT_DELAY } from '../../../../constants';
 import {
     validEndRecoveryForm, 
     validPassword, 
     validPasswordAgain
 } from './EndRecoveryFormState';
+import { useLoadingContext } from '../../../../contexts/LoadingContext/LoadingProvider';
+import { useResponseHandlerContext } from '../../../../contexts/ResponseHandlerContext/ResponseHandlerProvider';
 
 // данный основной блок является эксклюзивным и доступен
 // только со страницы окончания восстановления аккаунта
 
 export default function EndRecoveryMainBlock({user, ...props}) {
+    const { startLoading, stopLoading } = useLoadingContext();
+    const { resetResult, makePostRequest } = useResponseHandlerContext();
     const [tips, setTips] = useState({}); // подсказки для пользователя
 
     // новые данные аккаунта для восстановления
@@ -60,12 +65,33 @@ export default function EndRecoveryMainBlock({user, ...props}) {
     }, [recoveredUserData.passwordAgain])
 
     // обработка формы
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();        
         if (!validEndRecoveryForm(setTips)) {
             return;
         }
 
+        // хэшируем новый пароль перед отправкой
+        let hashedPassword;
+        await bcrypt.hash(recoveredUserData.password, 10)
+        .then((response) => {
+            hashedPassword = response;
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+        // основная часть 
+        startLoading();
+        resetResult();
+        await makePostRequest(
+            END_RECOVERY_ROUTE, {
+                id: user.id,
+                username: recoveredUserData.username,
+                password: hashedPassword
+            }
+        );
+        stopLoading();  
     }
     
     return (
