e4eb63feeda14928456c12b62df4d18052f97f82
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-04 23:17:35 +0300

    Существенное улучшение стиля компонента представления пользователя.
    с использованием новой константы и нового компонента помощника - разделителя.

--- frontend/src/components/Collection/UsersCollection/UserItem/UserItem.jsx
+++ frontend/src/components/Collection/UsersCollection/UserItem/UserItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './UserItem.module.css'
 import MiniButton from '../../../Helper/MiniButton/MiniButton';
+import Spacer from '../../../Helper/Spacer/Spacer';
 
 export default function UserItem({user,
                                   aboutUser,
@@ -9,21 +10,25 @@ export default function UserItem({user,
     // в случае если пользователь не установил картинку - ставим
     // картинку по умолчанию из файлов проекта
     const userImg = !aboutUser.image
-                ? `${window.location.origin}/DefUserIcon.png`
-                :  aboutUser.image
+        ? `${window.location.origin}/DefUserIcon.png`
+        :  aboutUser.image    
 
     return (
         <div className={classes.UserItem} {...props}>
+            <Spacer sizeH='1px' sizeW='10px' />
             <img src={userImg} alt="Not found" />
+            <Spacer sizeH='1px' sizeW='10px' />
             <div>
                 <p>{aboutUser.name} {aboutUser.secondname}</p>
             </div>
+            <Spacer sizeH='1px' sizeW='10px' />
             {
                 button &&
                 <MiniButton
                     button={button}
-                    data={user} />                
-            }            
+                    data={user} /> 
+            } 
+            <Spacer sizeH='1px' sizeW='10px' />
         </div>
     )
 }
