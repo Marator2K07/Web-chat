e313c396af7430e313ef3fb735cbe9646450a050
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-05 12:39:50 +0300

    Метод навигации исправлен и перенесен в компонент пользователя.
    + реализация компонента подправлена под новые условия.

--- frontend/src/components/Collection/UsersCollection/UsersCollection.jsx
+++ frontend/src/components/Collection/UsersCollection/UsersCollection.jsx
@@ -2,30 +2,13 @@ import React from 'react'
 import classes from './UsersCollection.module.css'
 import './UsersCollectionCSSTransition.css'
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { MEDIUM_TIMEOUT, ANOTHER_USER_PAGE_URL } from '../../../constants';
-import { useNavigate } from 'react-router-dom';
-import { useUserContext } from '../../../contexts/UserContext/UserProvider';
-import { useNavigationContext } from '../../../contexts/NavigationContext/NavigationProvider';
+import { MEDIUM_TIMEOUT } from '../../../constants';
 import UserItemView from '../../View/UserItemView/UserItemView';
 
 export default function UsersCollection({users,
                                          clue,
                                          button,
                                          ...props}) {
-    const { goNavigationWithAnimation } = useNavigationContext();
-    const { user } = useUserContext();
-    const navigate = useNavigate();
-
-    // если выбрали себя, то идем на свою личную вкладку,
-    // иначе новая страница с другим пользователем
-    const navigateHandler = async (username) => {
-        if (user.username === username) {
-            goNavigationWithAnimation('personalBlock');
-        } else {
-            navigate(`${ANOTHER_USER_PAGE_URL}?username=${username}`);
-        }        
-    }
-
     return (
         <div className={classes.UsersCollection} {...props}>
             <TransitionGroup>
@@ -35,7 +18,7 @@ export default function UsersCollection({users,
                             timeout={MEDIUM_TIMEOUT}
                             classNames="UsersCollection">
                                 <UserItemView 
-                                    user={users[key]}
+                                    thisUser={users[key]}
                                     button={button} />
                         </CSSTransition>
                     ))}
--- frontend/src/components/View/UserItemView/UserItemView.jsx
+++ frontend/src/components/View/UserItemView/UserItemView.jsx
@@ -2,17 +2,39 @@ import React, { useState } from 'react'
 import classes from './UserItemView.module.css'
 import Loadable from '../../Helper/Loadable/Loadable';
 import { cookies } from '../../../contexts/CookieContext';
-import { ANOTHER_USER_PAGE_URL, COOKIES_USERNAME, GET_ABOUT_USER_ROUTE_END } from '../../../constants';
+import {
+    ANOTHER_USER_PAGE_URL, 
+    COOKIES_USERNAME, 
+    GET_ABOUT_USER_ROUTE_END
+} from '../../../constants';
 import UserItem from '../../Collection/UsersCollection/UserItem/UserItem';
+import { useNavigate } from 'react-router-dom';
+import { useNavigationContext } from '../../../contexts/NavigationContext/NavigationProvider';
+import { useUserContext } from '../../../contexts/UserContext/UserProvider';
 
-export default function UserItemView({user, button, ...props}) {
+export default function UserItemView({thisUser, button, ...props}) {
     const [aboutUser, loadAboutUser] = useState(null);
+    const { navigationBlocks, goNavigationWithAnimation } = useNavigationContext();
+    const { user } = useUserContext();
+    const navigate = useNavigate();
+
+    // если выбрали себя, то идем на свою личную вкладку,
+    // иначе новая страница с другим пользователем
+    function navigateHandler(username) {        
+        if (user.username === username) {
+            goNavigationWithAnimation(navigationBlocks.personalBlock);
+        } else {
+            navigate(`${ANOTHER_USER_PAGE_URL}?username=${username}`);
+        }        
+    }
 
     if (aboutUser) {
         return (
-            <div className={classes.AboutUserView} {...props}>
+            <div className={classes.AboutUserView}
+                onClick={() => navigateHandler(thisUser.username)}
+                {...props}>
                 <UserItem 
-                    user={user}
+                    user={thisUser}
                     aboutUser={aboutUser} 
                     button={button} />
             </div>
@@ -22,10 +44,8 @@ export default function UserItemView({user, button, ...props}) {
             <Loadable 
                 isWorking={cookies.get(COOKIES_USERNAME)}
                 propertyName={'aboutUser'}
-                getDataUrl={`${ANOTHER_USER_PAGE_URL}/${user.username}${GET_ABOUT_USER_ROUTE_END}`}
+                getDataUrl={`${ANOTHER_USER_PAGE_URL}/${thisUser.username}${GET_ABOUT_USER_ROUTE_END}`}
                 setDataFunc={loadAboutUser} />
         )
     }
-
-    
 }
