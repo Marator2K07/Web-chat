4dec7eb8be1dfbdadd0c75eb97bb05e9b3f5aabd
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-07 11:15:29 +0300

    Расширение функционала контекста подсказок.

--- frontend/src/contexts/TipsContext/TipsContext.js
+++ frontend/src/contexts/TipsContext/TipsContext.js
@@ -1,23 +1,32 @@
 import { useCallback, useState } from "react";
 
 export const useCreateTipsContext = function(props) {
+    const [activeInput, setActiveInput] = useState(null);
     const [tips, setTips] = useState({});
 
+    // пишем подсказку
     const addTip = useCallback((property, info) => {
-        // пишем подсказку
         setTips(prevState => ({
             ...prevState,
             [property]: info
         }));
     }, []);
 
+    // таким образом убираем подсказку
     const removeTip = useCallback((property) => {
-        // таким образом убираем подсказку
         setTips(nextState => {
             let {property, ...newTips} = nextState;
             return newTips;
         });
     }, []);
 
-    return { tips, addTip, removeTip };
+    const newActiveInput = useCallback((ref) => {
+        setActiveInput(ref);
+    }, []);
+
+    return { tips,
+             addTip,
+             removeTip,
+             activeInput,
+             newActiveInput };
 }
