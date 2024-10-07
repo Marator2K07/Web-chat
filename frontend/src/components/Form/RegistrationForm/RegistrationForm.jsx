3902a47382f697abfb7a19e7ddad374763776b42
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-07 20:29:03 +0300

    Обновлённый вид формы регистрации с учетом новых подсказок.

--- frontend/src/components/Form/RegistrationForm/RegistrationForm.jsx
+++ frontend/src/components/Form/RegistrationForm/RegistrationForm.jsx
@@ -1,38 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import classes from './RegistrationForm.module.css'
 import { REGISTRATION_FORM_NAME } from '../../../constants'
+import { useTipsContext } from '../../../contexts/TipsContext/TipsProvider';
 
 export default function RegistrationForm({formData,
                                           handleChange,
                                           handleSubmit,
-                                          ...props}) {
+                                          ...props}) { 
+    const inputUsernameRef = useRef(null);   
+    const inputEmailRef = useRef(null);   
+    const inputPasswordRef = useRef(null);   
+    const inputPasswordAgainRef = useRef(null);
+    const { newTipsCoordinates, resetState } = useTipsContext();
+
     return (
-        <div className={classes.RegistrationForm} {...props}>
+        <div className={classes.RegistrationForm}
+            onBlur={resetState}
+            {...props}>
             <form name={REGISTRATION_FORM_NAME}>
                 <h4>Придумайте имя аккаунта:</h4>
                 <input
+                    ref={inputUsernameRef}
                     type='username'
                     name='username'
                     value={formData.username}
-                    onChange={handleChange}/>
+                    onChange={handleChange} 
+                    onFocus={() => newTipsCoordinates(inputUsernameRef)} />
                 <h4>Задайте почту для привязки:</h4>
                 <input
+                    ref={inputEmailRef}
                     type='email'
                     name='email'
                     value={formData.email} 
-                    onChange={handleChange}/>
+                    onChange={handleChange}
+                    onFocus={() => newTipsCoordinates(inputEmailRef)} />
                 <h4>А теперь придумайте пароль:</h4>
                 <input
+                    ref={inputPasswordRef}
                     type='password'
                     name='password'
                     value={formData.password} 
-                    onChange={handleChange}/>
+                    onChange={handleChange}
+                    onFocus={() => newTipsCoordinates(inputPasswordRef)} />
                 <h4>И повторите его:</h4>
                 <input
+                    ref={inputPasswordAgainRef}
                     type='password'
                     name='passwordAgain'
                     value={formData.passwordAgain} 
-                    onChange={handleChange}/>
+                    onChange={handleChange} 
+                    onFocus={() => newTipsCoordinates(inputPasswordAgainRef)} />
                 <button type='button' onClick={handleSubmit}>
                     Зарегистрироваться
                 </button>
