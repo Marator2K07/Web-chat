b4a5faf9b461c0ec160a191a495ce0cbae55f5db
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-07 15:31:00 +0300

    Доработан контекст анимации для главного блока.
    + добавлен важный метод для визуальной интерпретации неудачности сабмита формы.

--- frontend/src/contexts/MainBlockAnimationContext/MainBlockAnimationContext.js
+++ frontend/src/contexts/MainBlockAnimationContext/MainBlockAnimationContext.js
@@ -1,24 +1,49 @@
 import { useCallback, useState } from "react"
-import { SHORT_DELAY } from "../../constants";
+import { MOST_SHORT_DELAY, SHORT_DELAY } from "../../constants";
 
 export const useCreateMainBlockAnimationContext = function() {
     const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
     const [opacity, setOpacity] = useState(1);
     const [duration, setDuration] = useState(SHORT_DELAY/3000);
 
     const initCondition = useCallback(() => {
-            setX(0);
-            setOpacity(1);   
+        setX(0);
+        setY(0);
+        setOpacity(1);   
     }, []); 
 
     const leftCondition = useCallback(() => {
-            setX("-28vw");
-            setOpacity(0);
+        setX("-25vw");
+        setOpacity(0);
     }, []);
 
     const rightCondition = useCallback(() => {
-            setX("28vw");
-            setOpacity(0);
+        setX("25vw");
+        setOpacity(0);
+    }, []);
+
+    const shake = useCallback(() => {
+        let min = -22;
+        let max = 22;
+        setX(Math.floor(Math.random() * (max - min) + min)+'px');
+        setY(Math.floor(Math.random() * (max - min) + min)+'px');
+        setTimeout(() => {
+            setX(Math.floor(Math.random() * (max - min) + min)+'px');
+            setY(Math.floor(Math.random() * (max - min) + min)+'px');
+        }, MOST_SHORT_DELAY);
+        setTimeout(() => {
+            setX(Math.floor(Math.random() * (max - min) + min)+'px');
+            setY(Math.floor(Math.random() * (max - min) + min)+'px');
+        }, MOST_SHORT_DELAY*2);
+        setTimeout(() => {
+            setX(Math.floor(Math.random() * (max - min) + min)+'px');
+            setY(Math.floor(Math.random() * (max - min) + min)+'px');
+        }, MOST_SHORT_DELAY*3);
+        setTimeout(() => {
+            setX(0);
+            setY(0);
+        }, MOST_SHORT_DELAY*4);
     }, []);
 
     const changeDuration = useCallback((duration) => {
@@ -26,10 +51,12 @@ export const useCreateMainBlockAnimationContext = function() {
     }, []);
 
     return { x,
+             y,
              opacity,
              duration,
              initCondition,
              leftCondition,
              rightCondition,
+             shake,
              changeDuration };
 }
