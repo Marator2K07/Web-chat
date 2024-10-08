455fcfe5e29c4bf8749bb5b2a0e8038c3a5618a7
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-08 09:37:36 +0300

    Приведение методов обработки формы начала восстановления к рабочему состоянию.

--- frontend/src/components/MainBlock/Elements/StartRecoveryMainBlock/StartRecoveryFormState.js
+++ frontend/src/components/MainBlock/Elements/StartRecoveryMainBlock/StartRecoveryFormState.js
@@ -1,17 +1,24 @@
 import { START_RECOVERY_FORM_NAME } from "../../../../constants";
 import { formEmailIsCorrect, formParamIsEmpty } from "../../../../utils";
 
-export function validStartRecoveryForm(setTips) {
+export function validStartRecoveryForm(problemHandler, addTip, removeTip) {
     let emailIsOk = !formParamIsEmpty(START_RECOVERY_FORM_NAME, 'email') &&
-                     validEmail(setTips);
-    return emailIsOk;
+                     validEmail(addTip, removeTip);
+    if (emailIsOk) {
+        return true;
+    } else {
+        problemHandler();
+        return false;
+    }
 }
 
-export function validEmail(setTips) {
+export function validEmail(addTip, removeTip) {
     return formEmailIsCorrect(
         START_RECOVERY_FORM_NAME,
         'email',
+        'email1',
         "Неверный формат",
-        setTips
+        addTip,
+        removeTip
     );
 }
