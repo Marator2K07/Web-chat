340b1aa7daa1d164b551e908259e7cf5bb7bf503
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-09 13:58:10 +0300

    Обновлены функции обработки формы окончания восстановления.
    в связи с новым типов подсказок.

--- frontend/src/components/MainBlock/Elements/EndRecoveryMainBlock/EndRecoveryFormState.js
+++ frontend/src/components/MainBlock/Elements/EndRecoveryMainBlock/EndRecoveryFormState.js
@@ -2,33 +2,78 @@ import { END_RECOVERY_FORM_NAME } from "../../../../constants";
 import {
     formParamIsEmpty,
     formParamNotSmall,
+    passwordComplicated,
     passwordIsRepeated 
 } from "../../../../utils";
 
-export function validEndRecoveryForm(setTips) {
-    let usernameIsOk = !formParamIsEmpty(END_RECOVERY_FORM_NAME, 'username');
+export function validEndRecoveryForm(problemHandler, addTip, removeTip) {
+    let usernameIsOk = !formParamIsEmpty(END_RECOVERY_FORM_NAME, 'username') &&
+                        validUsername(addTip, removeTip);
     let passwordIsOk = !formParamIsEmpty(END_RECOVERY_FORM_NAME, 'password') &&
-                        validPassword(setTips);
-    let passwordAgainIsOk = !formParamIsEmpty(END_RECOVERY_FORM_NAME,'passwordAgain') && 
-                             validPasswordAgain(setTips);
-    return usernameIsOk && passwordIsOk && passwordAgainIsOk;
+                        validPassword(addTip, removeTip);        
+    let passwordAgainIsOk = !formParamIsEmpty(END_RECOVERY_FORM_NAME, 'passwordAgain') &&
+                             validPasswordAgain(addTip, removeTip);
+    if (usernameIsOk && passwordIsOk && passwordAgainIsOk) {
+        return true;
+    } else {
+        problemHandler();
+        return false;
+    }
 }
 
-export function validPassword(setTips) {
+export function validUsername(addTip, removeTip) {
     return formParamNotSmall(
         END_RECOVERY_FORM_NAME,
-        'password',
-        "Пароль слишком короткий",
-        setTips
+        'username',
+        'username1',
+        "Слишком короткое имя для аккаунта",
+        addTip,
+        removeTip,
     );
 }
 
-export function validPasswordAgain(setTips) {
-    return passwordIsRepeated(
+export function validPassword(addTip, removeTip) {
+    console.log(addTip);
+    
+    let passComplicated = passwordComplicated(
+        END_RECOVERY_FORM_NAME,
+        'password',
+        'password1',
+        "Пароль должен содержать как минимум 8 символов, включая по крайней мере одну строчную букву, одну заглавную букву и одну цифру",
+        addTip,
+        removeTip
+    );
+    let passRepeated = passwordIsRepeated(
         END_RECOVERY_FORM_NAME,
         'password',
         'passwordAgain',
+        'passwordAgain1',
         "Пароли не совпадают",
-        setTips
+        addTip,
+        removeTip
     );
+    return passComplicated && passRepeated;
+}
+
+export function validPasswordAgain(addTip, removeTip) {
+
+    console.log(addTip);
+    let passRepeated = passwordIsRepeated(
+        END_RECOVERY_FORM_NAME,
+        'password',
+        'passwordAgain',
+        'passwordAgain1',
+        "Пароли не совпадают",
+        addTip,
+        removeTip
+    );
+    let passComplicated = passwordComplicated(
+        END_RECOVERY_FORM_NAME,
+        'passwordAgain',
+        'passwordAgain2',
+        "Пароль должен содержать как минимум 8 символов, включая по крайней мере одну строчную букву, одну заглавную букву и одну цифру",
+        addTip,
+        removeTip
+    );
+    return passRepeated && passComplicated;
 }
