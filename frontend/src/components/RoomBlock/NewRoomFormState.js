2b9b4c97ad4233743f0be924657ec7db1336c238
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-09 22:39:12 +0300

    Создан файл для отдельной обработки формы создания комнаты общения.
    предполагаемая реализация.

---
+++ frontend/src/components/RoomBlock/NewRoomFormState.js
@@ -0,0 +1,24 @@
+import { NEW_ROOM_FORM_NAME } from "../../constants";
+import { formParamIsEmpty, formParamNotSmall } from "../../utils";
+
+export function validNewRoomForm(problemHandler, addTip, removeTip) {
+    let roomNameIsOk = !formParamIsEmpty(NEW_ROOM_FORM_NAME, 'roomName') &&
+                        validRoomName(addTip, removeTip);
+    if (roomNameIsOk) {
+        return true;
+    } else {
+        problemHandler();
+        return false;
+    }
+}
+
+export function validRoomName(addTip, removeTip) {
+    return formParamNotSmall(
+        NEW_ROOM_FORM_NAME,
+        'roomName',
+        'roomName1',
+        "Слишком короткое название комнаты",
+        addTip,
+        removeTip
+    );
+}
