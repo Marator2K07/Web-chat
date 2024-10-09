d4c925d23b4e958a33ff32349ab2528e6a8cd4cf
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-09 16:20:27 +0300

    Добавлена обработка подсказки для ввода фамилии в форме личной страницы.

--- frontend/src/components/Form/UpdateAboutUserForm/UpdateAboutUserForm.jsx
+++ frontend/src/components/Form/UpdateAboutUserForm/UpdateAboutUserForm.jsx
@@ -1,39 +1,47 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import classes from './UpdateAboutUserForm.module.css'
 import HorizontalLayout from '../../Helper/HorizontalLayout/HorizontalLayout'
 import { UPDATE_ABOUT_USER_FORM_NAME } from '../../../constants'
+import { useTipsContext } from '../../../contexts/TipsContext/TipsProvider';
 
 export default function UpdateAboutUserForm({formData,
                                              handleChange,
                                              handleSubmit,
                                              handleCancel,
                                              ...props}) {
+    const inputSecondnameRef = useRef(null); 
+    const { newTipsCoordinates, resetState } = useTipsContext();
+                            
     return (
-        <div className={classes.UpdateAboutUserForm} {...props}>
+        <div className={classes.UpdateAboutUserForm}
+            onBlur={resetState}
+            {...props}>
             <form name={UPDATE_ABOUT_USER_FORM_NAME}>
                 <p>Имя:</p>
                 <input
                     type='name'
                     name='name'
                     value={formData.name}
-                    onChange={handleChange}/>
+                    onChange={handleChange} />
                 <p>Фамилия:</p>
                 <input
+                    ref={inputSecondnameRef}
                     type='text'
                     name='secondname'
                     value={formData.secondname}
-                    onChange={handleChange}/>
+                    onChange={handleChange}
+                    onFocus={() => newTipsCoordinates(inputSecondnameRef)} />
                 <p>Дата рождения:</p>
                 <input
                     type='date'
                     name='dateOfBirth'
                     value={formData.dateOfBirth}
-                    onChange={handleChange}/>
+                    onChange={handleChange} />
                 <p>Аватарка:</p>
                 <input
                     type='file'
                     name='image'
-                    onInput={handleChange}/>
+                    onInput={handleChange} />
                 <HorizontalLayout>
                     <button type='button' onClick={handleSubmit}>
                         Применить изменения
