858e467767f92648c6d8c658db1226c7765ceb1f
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-11 09:54:31 +0300

    Улучшение дизайна для компонента верхнего блока.

--- frontend/src/components/TopBlock/TopBlock.jsx
+++ frontend/src/components/TopBlock/TopBlock.jsx
@@ -1,6 +1,7 @@
 import { React, useEffect, useRef, useState } from 'react'
 import { motion } from "framer-motion";
-import classes from './TopBlock.module.css'
+import classes from './TopBlock.module.css';
+import Spacer from '../Helper/Spacer/Spacer';
 import { setMenuOffset } from '../../utils';
 import AccountImageButton from './AccountImageButton/AccountImageButton';
 import { useNavigationContext } from '../../contexts/NavigationContext/NavigationProvider';
@@ -21,6 +22,7 @@ const TopBlock = ({...props}) => {
 
     return (
         <div className={classes.TopBlock} {...props}>
+            <Spacer sizeH='10px' sizeW='30px' />    
             <div className='left'>
                 <p>AwesomeChat</p>
             </div>     
@@ -32,7 +34,8 @@ const TopBlock = ({...props}) => {
             <motion.div animate={{ width: descHeaderWidth+33 }} />                                 
             <div className='right'>            
                 <AccountImageButton />
-            </div>             
+            </div>
+            <Spacer sizeH='10px' sizeW='30px' />      
         </div>
     )
 }
