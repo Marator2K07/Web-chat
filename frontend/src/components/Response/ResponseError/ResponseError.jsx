450ad3f89b9d0867929b8365a7c52c210b761c9a
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-11 10:30:23 +0300

    Удаление устаревшего кода в компоненте ошибки.

--- frontend/src/components/Response/ResponseError/ResponseError.jsx
+++ frontend/src/components/Response/ResponseError/ResponseError.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import classes from './ResponseError.module.css'
+import classes from './ResponseError.module.css';
 import { useLoadingContext } from '../../../contexts/LoadingContext/LoadingProvider'
 
 export default function ResponseError({message, ...props}) {
@@ -7,20 +7,6 @@ export default function ResponseError({message, ...props}) {
 
     return (
         <div className={classes.ResponseError} {...props}>
-            {/* <div className='name'>
-                <p>
-                    Имя: <br/><span style={{color: PINK_EBONY_COLOR}}>
-                            {message.name}
-                    </span>
-                </p> 
-            </div>
-            <div className='code'>
-                <p>
-                    Код: <br/><span style={{color: PINK_EBONY_COLOR}}>
-                        {message.code}
-                    </span>
-                </p> 
-            </div> */}
             <div className='info'>
                 <h4>Сообщение:</h4>
                 <p>{message.message}</p> 
