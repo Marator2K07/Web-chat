90ba3de0c772fcf396a54997f315abcb22fb059e
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-12 23:45:54 +0300

    Новый помощник - в этот раз для вертикальной прокрутки.
    с предполагаемым дизайном.

---
+++ frontend/src/components/Helper/ScrollableHorizontal/ScrollableHorizontal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './ScrollableHorizontal.module.css'
+
+export default function ScrollableHorizontal({...props}) {
+    return (
+        <div 
+            className={classes.ScrollableHorizontal} 
+            {...props}>
+            
+        </div>
+    )
+}
