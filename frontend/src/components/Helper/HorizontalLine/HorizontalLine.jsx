b183d5a0276f5d92554fdc1f1b9ffb2b1e0dc6cd
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-12 12:15:30 +0300

    Созданы еще двое компонента помощников - для отображения подкомпонентов в линию.
    сразу с начальными стилями.

---
+++ frontend/src/components/Helper/HorizontalLine/HorizontalLine.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './HorizontalLine.module.css'
+
+export default function HorizontalLine({...props}) {
+    return (
+        <div className={classes.HorizontalLine} {...props}>
+            
+        </div>
+    )
+}
