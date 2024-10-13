6c9878677c154c07728ed638eb80fd82322be00d
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-13 10:55:50 +0300

    Добавили возможность драг энд роуп событий на компонент гор. прокрутки в целом.
    , а не только на одну полосу прокрутки. Заодно убрали выделение текста с этого компонента.

--- frontend/src/components/Helper/ScrollableHorizontal/ScrollableHorizontal.jsx
+++ frontend/src/components/Helper/ScrollableHorizontal/ScrollableHorizontal.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import classes from './ScrollableHorizontal.module.css'
 
 export default function ScrollableHorizontal({...props}) {
+    const thisComponent = useRef(null);
+    const [dragging, setDragging] = useState(false);
+    const [startX, setStartX] = useState();
+    const [scrollX, setScrollX] = useState();
+
     return (
         <div 
+            ref={thisComponent}
             className={classes.ScrollableHorizontal} 
+            onMouseDown={(e) => {
+                setDragging(true);
+                setStartX(e.pageX - thisComponent.current.offsetLeft);
+                setScrollX(thisComponent.current.scrollLeft);
+            }}
+            onMouseUp={() => setDragging(false)}
+            onMouseMove={(e) => {
+                if (dragging) {
+                    e.preventDefault();
+                    let y = e.pageX;
+                    const walk = (y-startX) * 2;
+                    thisComponent.current.scrollLeft = scrollX - walk;
+                }
+            }}
             {...props}>
             
         </div>
