04fd99cfc78146df6219b3473680079e7684590c
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-13 21:24:37 +0300

    Компонент вертикальной прокрутки теперь активно пользуется связанным контекстом.

--- frontend/src/components/Helper/ScrollableVertical/ScrollableVertical.jsx
+++ frontend/src/components/Helper/ScrollableVertical/ScrollableVertical.jsx
@@ -1,7 +1,9 @@
 import React, { useRef, useState } from 'react'
 import classes from './ScrollableVertical.module.css'
+import { useScrollContext } from '../../../contexts/ScrollContext/ScrollProvider';
 
 export default function ScrollableVertical({...props}) {
+    const { handleScroll } = useScrollContext();
     const thisComponent = useRef(null);
     const [dragging, setDragging] = useState(false);
     const [startY, setStartY] = useState();
@@ -15,15 +17,29 @@ export default function ScrollableVertical({...props}) {
                 setDragging(true);
                 setStartY(e.pageY - thisComponent.current.offsetTop);
                 setScrollY(thisComponent.current.scrollTop);
+                handleScroll(thisComponent);
             }}
-            onMouseUp={() => setDragging(false)}
+            onMouseUp={() => {
+                setDragging(false);
+                handleScroll(thisComponent);
+            }}
             onMouseMove={(e) => {
                 if (dragging) {
                     e.preventDefault();
                     let y = e.pageY;
                     const walk = (y-startY) * 2;
                     thisComponent.current.scrollTop = scrollY - walk;
+                    // управляем контекстом при драг энд ропе
+                    handleScroll(thisComponent);
                 }
+            }}
+            onWheelCapture={() => {
+                // также управляем контекстом при обычной прокрутке
+                handleScroll(thisComponent);
+            }}
+            onResize={() => {
+                // еще управляем контекстом при изменении размеров компонента
+                handleScroll(thisComponent);
             }}
             {...props}>
             
