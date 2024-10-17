8d0efe1ea22aabac1c8b60f0a3650011a8bfa690
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-17 21:06:26 +0300

    Перенос метода навигации при нажатии на пользователя на уровень ниже.

--- frontend/src/components/Collection/UsersCollection/UserItem/UserItem.jsx
+++ frontend/src/components/Collection/UsersCollection/UserItem/UserItem.jsx
@@ -2,19 +2,38 @@ import React from 'react'
 import classes from './UserItem.module.css'
 import MiniButton from '../../../Helper/MiniButton/MiniButton';
 import Spacer from '../../../Helper/Spacer/Spacer';
+import { useNavigate } from 'react-router-dom';
+import { ANOTHER_USER_PAGE_URL } from '../../../../constants';
+import { useNavigationContext } from '../../../../contexts/NavigationContext/NavigationProvider';
+import { useUserContext } from '../../../../contexts/UserContext/UserProvider';
 
 export default function UserItem({user,
                                   aboutUser,
                                   button,
                                   ...props}) {
+    const { navigationBlocks, goNavigationWithAnimation } = useNavigationContext();
+    const { user: thisUser } = useUserContext();
+    const navigate = useNavigate();
+
     // в случае если пользователь не установил картинку - ставим
     // картинку по умолчанию из файлов проекта
     const userImg = !aboutUser.image
         ? `${window.location.origin}/DefUserIcon.png`
         :  aboutUser.image    
 
+    // навигация при нажатии на обьект загруженного юзера
+    function navigateHandler(username) {        
+        if (user.username === username) {
+            goNavigationWithAnimation(navigationBlocks.personalBlock);
+        } else {
+            navigate(`${ANOTHER_USER_PAGE_URL}?username=${username}`);
+        }        
+    }
+
     return (
-        <div className={classes.UserItem} {...props}>
+        <div className={classes.UserItem}
+            onClick={() => navigateHandler(thisUser.username)}
+            {...props}>
             <Spacer sizeH='1px' sizeW='10px' />
             <img src={userImg} alt="Not found" />
             <Spacer sizeH='1px' sizeW='10px' />
--- frontend/src/components/View/UserItemView/UserItemView.jsx
+++ frontend/src/components/View/UserItemView/UserItemView.jsx
@@ -8,31 +8,13 @@ import {
     GET_ABOUT_USER_ROUTE_END
 } from '../../../constants';
 import UserItem from '../../Collection/UsersCollection/UserItem/UserItem';
-import { useNavigate } from 'react-router-dom';
-import { useNavigationContext } from '../../../contexts/NavigationContext/NavigationProvider';
-import { useUserContext } from '../../../contexts/UserContext/UserProvider';
 
 export default function UserItemView({thisUser, button, ...props}) {
     const [aboutUser, loadAboutUser] = useState(null);
-    const { navigationBlocks, goNavigationWithAnimation } = useNavigationContext();
-    const { user } = useUserContext();
-    const navigate = useNavigate();
-
-    // если выбрали себя, то идем на свою личную вкладку,
-    // иначе новая страница с другим пользователем
-    function navigateHandler(username) {        
-        if (user.username === username) {
-            goNavigationWithAnimation(navigationBlocks.personalBlock);
-        } else {
-            navigate(`${ANOTHER_USER_PAGE_URL}?username=${username}`);
-        }        
-    }
 
     if (aboutUser) {
         return (
-            <div className={classes.AboutUserView}
-                onClick={() => navigateHandler(thisUser.username)}
-                {...props}>
+            <div className={classes.AboutUserView} {...props}>
                 <UserItem 
                     user={thisUser}
                     aboutUser={aboutUser} 
