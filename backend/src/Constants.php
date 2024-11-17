c9e8a95b52d3d134cbdb1d92ded530cb285052f5
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-17 17:18:23 +0300

    Изменение определения констант + добавление новых.

--- backend/src/Constants.php
+++ backend/src/Constants.php
@@ -1,18 +1,23 @@
 <?php
 
-namespace App\Constants;
+define("EMPTY_DELAY", 0);
+define("SHORT_DELAY", 1250);
+define("AVERAGE_DELAY", 2500);
+define("LONG_DELAY", 3750);
+define("EXTRA_LONG_DELAY", 7000);
 
-class Constants
-{
-    const EMPTY_DELAY = 0;
-    const SHORT_DELAY = 1250;
-    const AVERAGE_DELAY = 2500;
-    const LONG_DELAY = 3750;
-    const EXTRA_LONG_DELAY = 7000;
-    const HOST_RECEIVER_PORT = 3000;
-    const MAIN_TAG = 'h3';
-    const DEFAULT_SEARCH_SIZE = 3;
-    const NAME_SEARCH_TAG = 'name';
-    const USERNAME_SEARCH_TAG = 'username';
-    const SECONDNAME_SEARCH_TAG = 'secondname';
-}
+define("HOST_RECEIVER_PORT", 3000);
+
+define("MAIN_TAG", "h3");
+define("NAME_TAG", "name");
+define("SECONDNAME_TAG", "secondname");
+define("BIRTHDAY_TAG","dateOfBirth");
+define("CONFIRMTOKEN_TAG","confirmToken");
+define("IMAGE_TAG","image");
+
+define("DEFAULT_SEARCH_SIZE", 3);
+define("NAME_SEARCH_TAG", "name");
+define("USERNAME_SEARCH_TAG", "username");
+define("SECONDNAME_SEARCH_TAG", "secondname");
+
+define("INVALID_DATE","Invalid Date");
