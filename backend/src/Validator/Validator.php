d02b24940c361588b4960596063be4dce429a7c6
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-17 17:56:30 +0300

    Создали свой интерфейс валидации для удобства.

---
+++ backend/src/Validator/Validator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Src\Validator;
+
+interface Validator
+{
+    /**
+     * Валидация данных для сущности определнного типа
+     * @param array $data входящие данные для проверки
+     * @return mixed массив ошибок при плохом исходе и истину при корректном
+     */
+    public function validate(array $data): mixed;
+}
