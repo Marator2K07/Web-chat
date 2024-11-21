ac3268696acba52e4424da244bf5aa775072e501
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-21 13:42:30 +0300

    Теперь реализация валидации для формы начала восстановления на стороне сервера.
    + удаление ныне не нужных файлов.

--- frontend/src/components/Form/StartRecoveryForm/StartRecoveryForm.jsx
+++ frontend/src/components/Form/StartRecoveryForm/StartRecoveryForm.jsx
@@ -2,9 +2,11 @@ import React, { useRef } from 'react'
 import classes from './StartRecoveryForm.module.css'
 import { START_RECOVERY_FORM_NAME } from '../../../constants'
 import { useTipsContext } from '../../../contexts/TipsContext/TipsProvider';
+import TipsCollection from '../../Collection/TipsCollection/TipsCollection';
 
 export default function StartRecoveryForm({
     formData,
+    errorsData,
     handleEmailChange,
     handleSubmit,
     ...props
@@ -28,6 +30,12 @@ export default function StartRecoveryForm({
                     onChange={handleEmailChange}
                     onFocus={() => newTipsCoordinates(inputEmailRef)}
                 />
+                {
+                    errorsData &&
+                    errorsData['email'] &&
+                    <TipsCollection tips={errorsData['email']} />
+                }                
+
                 <p>Важное замечание: восстановление работает
                     только с активированными аккаунтами.</p>
                 <button type='button' onClick={handleSubmit}>
--- frontend/src/components/MainBlock/Elements/StartRecoveryMainBlock/StartRecoveryMainBlock.jsx
+++ frontend/src/components/MainBlock/Elements/StartRecoveryMainBlock/StartRecoveryMainBlock.jsx
@@ -1,20 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import classes from './StartRecoveryMainBlock.module.css';
-import StartRecoveryForm from '../../../Form/StartRecoveryForm/StartRecoveryForm';
-import { EXTRA_SHORT_DELAY, START_RECOVERY_ROUTE } from '../../../../constants';
-import { validEmail, validStartRecoveryForm } from './StartRecoveryFormState';
+import { START_RECOVERY_ROUTE } from '../../../../constants';
 import { useLoadingContext } from '../../../../contexts/LoadingContext/LoadingProvider';
 import { useResponseHandlerContext } from '../../../../contexts/ResponseHandlerContext/ResponseHandlerProvider';
 import { useMainBlockAnimationContext } from '../../../../contexts/MainBlockAnimationContext/MainBlockAnimationProvider';
-import { useTipsContext } from '../../../../contexts/TipsContext/TipsProvider';
+import StartRecoveryForm from '../../../Form/StartRecoveryForm/StartRecoveryForm';
 
 export default function StartRecoveryMainBlock({ ...props }) {
     const { startLoading, stopLoading } = useLoadingContext();
     const { resetResult, makePostRequest } = useResponseHandlerContext();
     const { shake } = useMainBlockAnimationContext();
-    const { addTip, removeTip } = useTipsContext();
 
-    // данные формы
+    const [validationErrors, setValidationErrors] = useState([]);
     const [recoveryInfo, setRecoveryInfo] = useState({
         email: ''
     });
@@ -27,27 +24,21 @@ export default function StartRecoveryMainBlock({ ...props }) {
         });
     }
 
-    // проверка спустя паузу корректности ввода емайла
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            recoveryInfo.email !== "" && validEmail(addTip, removeTip);
-        }, EXTRA_SHORT_DELAY);
-        return () => clearTimeout(timeout)
-    }, [recoveryInfo.email, addTip, removeTip])
-
     // обработка формы
     async function handleSubmit(e) {
         e.preventDefault();
-        if (!validStartRecoveryForm(shake, addTip, removeTip)) {
-            return;
-        }
-        // основная часть
         startLoading();
         resetResult();
         await makePostRequest(
             START_RECOVERY_ROUTE, {
-            email: recoveryInfo.email,
-        }
+                email: recoveryInfo.email,
+            },
+            async (response) => {
+                setValidationErrors(response.data.validationErrors);
+                if (response.data.validationErrors) {
+                    shake();
+                }
+            }
         );
 
         stopLoading();
@@ -57,6 +48,7 @@ export default function StartRecoveryMainBlock({ ...props }) {
         <div className={classes.StartRecoveryMainBlock} {...props}>
             <StartRecoveryForm
                 formData={recoveryInfo}
+                errorsData={validationErrors}
                 handleEmailChange={handleEmailChange}
                 handleSubmit={handleSubmit}
             />
